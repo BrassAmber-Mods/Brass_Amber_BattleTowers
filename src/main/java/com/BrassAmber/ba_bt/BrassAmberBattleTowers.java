4a0e41ecf54efbc23d8b2f3dfd84d4dd2953a050
Milamber <socky79.turbulance@gmail.com>
2021-09-29 01:46:01 -0400

    Initial Repo Setup

---
+++ src/main/java/com/BrassAmber/ba_bt/BrassAmberBattleTowers.java
@@ -0,0 +1,182 @@
+package com.BrassAmber.ba_bt;
+
+
+import com.mojang.serialization.Codec;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.FlatChunkGenerator;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.coremod.CoreModTracker;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BrassAmberBattleTowers.MOD_ID)
+public class BrassAmberBattleTowers
+{
+    public static final String MOD_ID = "ba_bt";
+
+    // Directly reference a log4j logger
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public BrassAmberBattleTowers() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        BTStructures.DEFERRED_REGISTRY_STRUCTURE.register(eventBus);
+
+        eventBus.addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        eventBus.addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        eventBus.addListener(this::processIMC);
+        // Register the doClientStuff method for modloading
+        eventBus.addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // For events that happen after initialization. This is probably going to be use a lot.
+        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+        forgeBus.addListener(EventPriority.NORMAL, this::addDimensionalSpacing);
+
+        // The comments for BiomeLoadingEvent and StructureSpawnListGatherEvent says to do HIGH for additions.
+        forgeBus.addListener(EventPriority.HIGH, this::biomeModification);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
+        event.enqueueWork(() -> {
+            BTStructures.setupStructures();
+            BTConfiguredStructures.registerConfiguredStructures();
+        });
+    }
+
+    public void biomeModification(final BiomeLoadingEvent event) {
+        /*
+         * Add our structure to all biomes including other modded biomes.
+         * You can skip or add only to certain biomes based on stuff like biome category,
+         * temperature, scale, precipitation, mod id, etc. All kinds of options!
+         *
+         * You can even use the BiomeDictionary as well! To use BiomeDictionary, do
+         * RegistryKey.getOrCreateKey(Registry.BIOME_KEY, event.getName()) to get the biome's
+         * registrykey. Then that can be fed into the dictionary to get the biome's types.
+         */
+        if (event.getCategory() == Biome.Category.BEACH || event.getCategory() == Biome.Category.EXTREME_HILLS ||
+                event.getCategory() == Biome.Category.PLAINS || event.getCategory() == Biome.Category.SAVANNA)
+            event.getGeneration().getStructures().add(() -> BTConfiguredStructures.CONFIGURED_LAND_BATTLE_TOWER);
+    }
+
+    private static Method GETCODEC_METHOD;
+    public void addDimensionalSpacing(final WorldEvent.Load event) {
+        if(event.getWorld() instanceof ServerWorld){
+            ServerWorld serverWorld = (ServerWorld)event.getWorld();
+
+            /*
+             * Skip Terraforged's chunk generator as they are a special case of a mod locking down their chunkgenerator.
+             * They will handle your structure spacing for your if you add to WorldGenRegistries.NOISE_GENERATOR_SETTINGS in your structure's registration.
+             * This here is done with reflection as this tutorial is not about setting up and using Mixins.
+             * If you are using mixins, you can call the codec method with an invoker mixin instead of using reflection.
+             */
+            try {
+                if(GETCODEC_METHOD == null) GETCODEC_METHOD = ObfuscationReflectionHelper.findMethod(ChunkGenerator.class, "func_230347_a_");
+                ResourceLocation cgRL = Registry.CHUNK_GENERATOR.getKey((Codec<? extends ChunkGenerator>) GETCODEC_METHOD.invoke(serverWorld.getChunkSource().generator));
+                if(cgRL != null && cgRL.getNamespace().equals("terraforged")) return;
+            }
+            catch(Exception e){
+                BrassAmberBattleTowers.LOGGER.error("Was unable to check if " + serverWorld.dimension().location() + " is using Terraforged's ChunkGenerator.");
+            }
+
+            /*
+             * Prevent spawning our structure in Vanilla's superflat world as
+             * people seem to want their superflat worlds free of modded structures.
+             * Also that vanilla superflat is really tricky and buggy to work with in my experience.
+             */
+            if(serverWorld.getChunkSource().getGenerator() instanceof FlatChunkGenerator &&
+                    serverWorld.dimension().equals(World.OVERWORLD)){
+                return;
+            }
+
+            /*
+             * putIfAbsent so people can override the spacing with dimension datapacks themselves if they wish to customize spacing more precisely per dimension.
+             * Requires AccessTransformer  (see resources/META-INF/accesstransformer.cfg)
+             *
+             * NOTE: if you add per-dimension spacing configs, you can't use putIfAbsent as WorldGenRegistries.NOISE_GENERATOR_SETTINGS in FMLCommonSetupEvent
+             * already added your default structure spacing to some dimensions. You would need to override the spacing with .put(...)
+             * And if you want to do dimension blacklisting, you need to remove the spacing entry entirely from the map below to prevent generation safely.
+             */
+            Map<Structure<?>, StructureSeparationSettings> tempMap = new HashMap(serverWorld.getChunkSource().generator.getSettings().structureConfig());
+            tempMap.putIfAbsent(BTStructures.LAND_BATTLE_TOWER.get(), DimensionStructuresSettings.DEFAULTS.get(BTStructures.LAND_BATTLE_TOWER.get()));
+            serverWorld.getChunkSource().generator.getSettings().structureConfig = tempMap;
+        }
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        // some example code to dispatch IMC to another mod
+        InterModComms.sendTo("ba_bt", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+        // some example code to receive and process InterModComms from other mods
+        LOGGER.info("Got IMC {}", event.getIMCStream().
+                map(m->m.getMessageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
+}
---
+++ src/main/java/com/BrassAmber/ba_bt/structures/LandBattleTower.java
@@ -0,0 +1,158 @@
+package com.BrassAmber.ba_bt.structures;
+
+import com.BrassAmber.ba_bt.BrassAmberBattleTowers;
+import com.mojang.serialization.Codec;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SharedSeedRandom;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.vector.Vector3i;
+import net.minecraft.util.registry.DynamicRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.provider.BiomeProvider;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.jigsaw.JigsawManager;
+import net.minecraft.world.gen.feature.structure.*;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+import org.apache.logging.log4j.Level;
+
+import java.util.List;
+import java.util.Random;
+
+public class LandBattleTower extends Structure<NoFeatureConfig> {
+    public LandBattleTower(Codec<NoFeatureConfig> codec) {
+        super(codec);
+    }
+
+    @Override
+    public IStartFactory<NoFeatureConfig> getStartFactory() {
+        return LandBattleTower.Start::new;
+    }
+
+    @Override
+    public GenerationStage.Decoration step() {
+        return GenerationStage.Decoration.SURFACE_STRUCTURES;
+    }
+
+    protected boolean isFeatureChunk(ChunkGenerator chunkGenerator, BiomeProvider biomeSource, long seed, SharedSeedRandom chunkRandom, int chunkX, int chunkZ, Biome biome, ChunkPos chunkPos, NoFeatureConfig featureConfig) {
+        BlockPos centerOfChunk = new BlockPos(chunkX * 16, 0, chunkZ * 16);
+
+        // Grab height of land. Will stop at first non-air block.
+        int landHeight = chunkGenerator.getFirstOccupiedHeight(centerOfChunk.getX(), centerOfChunk.getZ(), Heightmap.Type.WORLD_SURFACE_WG);
+
+        // Grabs column of blocks at given position. In overworld, this column will be made of stone, water, and air.
+        // In nether, it will be netherrack, lava, and air. End will only be endstone and air. It depends on what block
+        // the chunk generator will place for that dimension.
+        IBlockReader columnOfBlocks = chunkGenerator.getBaseColumn(centerOfChunk.getX(), centerOfChunk.getZ());
+
+        // Combine the column of blocks with land height and you get the top block itself which you can test.
+        BlockState topBlock = columnOfBlocks.getBlockState(centerOfChunk.above(landHeight));
+
+        // Now we test to make sure our structure is not spawning on water or other fluids.
+        // You can do height check instead too to make it spawn at high elevations.
+        return topBlock.getFluidState().isEmpty(); //landHeight > 100;
+    }
+
+    public static class Start extends StructureStart<NoFeatureConfig> {
+        public Start(Structure<NoFeatureConfig> structureIn, int chunkX, int chunkZ, MutableBoundingBox mutableBoundingBox, int referenceIn, long seedIn) {
+            super(structureIn, chunkX, chunkZ, mutableBoundingBox, referenceIn, seedIn);
+        }
+
+        public void generatePieces(DynamicRegistries dynamicRegistryManager, ChunkGenerator chunkGenerator, TemplateManager templateManagerIn, int chunkX, int chunkZ, Biome biomeIn, NoFeatureConfig config) {
+
+            // Turns the chunk coordinates into actual coordinates we can use
+            int x = chunkX * 16;
+            int z = chunkZ * 16;
+
+            /*
+             * We pass this into addPieces to tell it where to generate the structure.
+             * If addPieces's last parameter is true, blockpos's Y value is ignored and the
+             * structure will spawn at terrain height instead. Set that parameter to false to
+             * force the structure to spawn at blockpos's Y value instead. You got options here!
+             */
+            BlockPos centerPos = new BlockPos(x, 0, z);
+
+            /*
+             * If you are doing Nether structures, you'll probably want to spawn your structure on top of ledges.
+             * Best way to do that is to use getBaseColumn to grab a column of blocks at the structure's x/z position.
+             * Then loop through it and look for land with air above it and set blockpos's Y value to it.
+             * Make sure to set the final boolean in JigsawManager.addPieces to false so
+             * that the structure spawns at blockpos's y value instead of placing the structure on the Bedrock roof!
+             */
+            //IBlockReader blockReader = chunkGenerator.getBaseColumn(blockpos.getX(), blockpos.getZ());
+
+            // All a structure has to do is call this method to turn it into a jigsaw based structure!
+            JigsawManager.addPieces(
+                    dynamicRegistryManager,
+                    new VillageConfig(() -> dynamicRegistryManager.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY)
+                            // The path to the starting Template Pool JSON file to read.
+
+                            // Note, this is "structure_tutorial:run_down_house/start_pool" which means
+                            // the game will automatically look into the following path for the template pool:
+                            // "resources/data/structure_tutorial/worldgen/template_pool/run_down_house/start_pool.json"
+                            // This is why your pool files must be in "data/<modid>/worldgen/template_pool/<the path to the pool here>"
+                            // because the game automatically will check in worldgen/template_pool for the pools.
+                            .get(new ResourceLocation(BrassAmberBattleTowers.MOD_ID, "land_tower/start_pool")),
+
+                            // How many pieces outward from center can a recursive jigsaw structure spawn.
+                            // Our structure is only 1 piece outward and isn't recursive so any value of 1 or more doesn't change anything.
+                            // However, I recommend you keep this a decent value like 10 so people can use datapacks to add additional pieces to your structure easily.
+                            // But don't make it too large for recursive structures like villages or you'll crash server due to hundreds of pieces attempting to generate!
+                            10),
+                    AbstractVillagePiece::new,
+                    chunkGenerator,
+                    templateManagerIn,
+                    centerPos, // Position of the structure. Y value is ignored if last parameter is set to true.
+                    this.pieces, // The list that will be populated with the jigsaw pieces after this method.
+                    this.random,
+                    false, // Special boundary adjustments for villages. It's... hard to explain. Keep this false and make your pieces not be partially intersecting.
+                    // Either not intersecting or fully contained will make children pieces spawn just fine. It's easier that way.
+                    true);  // Place at heightmap (top land). Set this to false for structure to be place at the passed in blockpos's Y value instead.
+            // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
+
+
+            // **THE FOLLOWING TWO LINES ARE OPTIONAL**
+            //
+            // Right here, you can do interesting stuff with the pieces in this.pieces such as offset the
+            // center piece by 50 blocks up for no reason, remove repeats of a piece or add a new piece so
+            // only 1 of that piece exists, etc. But you do not have access to the piece's blocks as this list
+            // holds just the piece's size and positions. Blocks will be placed later in JigsawManager.
+            //
+            // In this case, we do `piece.offset` to raise pieces up by 1 block so that the house is not right on
+            // the surface of water or sunken into land a bit.
+            //
+            // Then we extend the bounding box down by 1 by doing `piece.getBoundingBox().minY` which will cause the
+            // land formed around the structure to be lowered and not cover the doorstep. You can raise the bounding
+            // box to force the structure to be buried as well. This bounding box stuff with land is only for structures
+            // that you added to Structure.NOISE_AFFECTING_FEATURES field handles adding land around the base of structures.
+
+            // Since by default, the start piece of a structure spawns with it's corner at centerPos
+            // and will randomly rotate around that corner, we will center the piece on centerPos instead.
+            // This is so that our structure's start piece is now centered on the water check done in isFeatureChunk.
+            // Whatever the offset done to center the start piece, that offset is applied to all other pieces
+            // so the entire structure is shifted properly to the new spot.
+            Vector3i structureCenter = this.pieces.get(0).getBoundingBox().getCenter();
+            int xOffset = centerPos.getX() - structureCenter.getX();
+            int zOffset = centerPos.getZ() - structureCenter.getZ();
+            for (StructurePiece structurePiece : this.pieces) {
+                structurePiece.move(xOffset, 0, zOffset);
+            }
+
+            // Sets the bounds of the structure once you are finished.
+            this.calculateBoundingBox();
+
+            // I use to debug and quickly find out if the structure is spawning or not and where it is.
+            // This is returning the coordinates of the center starting piece.
+            BrassAmberBattleTowers.LOGGER.log(Level.DEBUG, "Land Battle Tower at " + this.pieces.get(0).getBoundingBox().x0 + " "
+                    + this.pieces.get(0).getBoundingBox().y0 + " " + this.pieces.get(0).getBoundingBox().z0);
+        }
+
+    }
+}
