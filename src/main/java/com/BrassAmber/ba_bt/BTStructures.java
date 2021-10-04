4e9e02309ce50272055aed1589f12ea6cc259951
JMilamber <stringerj@allegheny.edu>
2021-10-04 03:21:41 -0400

    Added abandoned Barracks rooms

--- src/main/java/com/BrassAmber/ba_bt/BTStructures.java
+++ src/main/java/com/BrassAmber/ba_bt/BTStructures.java
@@ -28,8 +28,8 @@ public class BTStructures {
     public static void setupStructures() {
         setupMapSpacingAndLand(
                 LAND_BATTLE_TOWER.get(), /* The instance of the structure */
-                new StructureSeparationSettings(15 /* average distance apart in chunks between spawn attempts */,
-                        10 /* minimum distance apart in chunks between spawn attempts. MUST BE LESS THAN ABOVE VALUE*/,
+                new StructureSeparationSettings(10 /* average distance apart in chunks between spawn attempts */,
+                        8 /* minimum distance apart in chunks between spawn attempts. MUST BE LESS THAN ABOVE VALUE*/,
                         1234567890 /* this modifies the seed of the structure so no two structures always spawn over each-other. Make this large and unique. */),
                 false);
 
