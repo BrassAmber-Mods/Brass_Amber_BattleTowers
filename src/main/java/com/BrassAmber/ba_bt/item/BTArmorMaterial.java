8b19396c9a11578b1a38a3e9b42d2af513aa3d93
Xratedjunior <marnix@roomer.tv>
2021-10-20 15:22:44 +0200

    Implement '@Override' in BTArmorMaterial.
    + Will help us find errors when updating the mod in future versions.

--- src/main/java/com/BrassAmber/ba_bt/item/BTArmorMaterial.java
+++ src/main/java/com/BrassAmber/ba_bt/item/BTArmorMaterial.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import com.BrassAmber.ba_bt.BrassAmberBattleTowers;
 
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.LazyValue;
@@ -13,6 +14,9 @@ import net.minecraft.util.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+/**
+ * Reference from {@link ArmorMaterial}
+ */
 public enum BTArmorMaterial implements IArmorMaterial {
 	SILVER("silver", 11, new int[] { 2, 5, 6, 2 }, 20, SoundEvents.ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> {
 		return Ingredient.of(BTItems.SILVER_INGOT);
@@ -28,46 +32,54 @@ public enum BTArmorMaterial implements IArmorMaterial {
 	private final float knockbackResistance;
 	private final LazyValue<Ingredient> repairIngredient;
 
-	private BTArmorMaterial(String p_i231593_3_, int p_i231593_4_, int[] p_i231593_5_, int p_i231593_6_, SoundEvent p_i231593_7_, float p_i231593_8_, float p_i231593_9_, Supplier<Ingredient> p_i231593_10_) {
-		this.name = p_i231593_3_;
-		this.durabilityMultiplier = p_i231593_4_;
-		this.slotProtections = p_i231593_5_;
-		this.enchantmentValue = p_i231593_6_;
-		this.sound = p_i231593_7_;
-		this.toughness = p_i231593_8_;
-		this.knockbackResistance = p_i231593_9_;
-		this.repairIngredient = new LazyValue<>(p_i231593_10_);
+	private BTArmorMaterial(String name, int durabilityMultiplier, int[] slotProtections, int enchantmentValue, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+		this.name = name;
+		this.durabilityMultiplier = durabilityMultiplier;
+		this.slotProtections = slotProtections;
+		this.enchantmentValue = enchantmentValue;
+		this.sound = equipSound;
+		this.toughness = toughness;
+		this.knockbackResistance = knockbackResistance;
+		this.repairIngredient = new LazyValue<>(repairIngredient);
 	}
 
-	public int getDurabilityForSlot(EquipmentSlotType p_200896_1_) {
-		return HEALTH_PER_SLOT[p_200896_1_.getIndex()] * this.durabilityMultiplier;
+	@Override
+	public int getDurabilityForSlot(EquipmentSlotType equipmentSlotType) {
+		return HEALTH_PER_SLOT[equipmentSlotType.getIndex()] * this.durabilityMultiplier;
 	}
 
-	public int getDefenseForSlot(EquipmentSlotType p_200902_1_) {
-		return this.slotProtections[p_200902_1_.getIndex()];
+	@Override
+	public int getDefenseForSlot(EquipmentSlotType equipmentSlotType) {
+		return this.slotProtections[equipmentSlotType.getIndex()];
 	}
 
+	@Override
 	public int getEnchantmentValue() {
 		return this.enchantmentValue;
 	}
 
+	@Override
 	public SoundEvent getEquipSound() {
 		return this.sound;
 	}
 
+	@Override
 	public Ingredient getRepairIngredient() {
 		return this.repairIngredient.get();
 	}
 
+	@Override
 	@OnlyIn(Dist.CLIENT)
 	public String getName() {
 		return BrassAmberBattleTowers.MOD_ID + ":" + this.name;
 	}
 
+	@Override
 	public float getToughness() {
 		return this.toughness;
 	}
 
+	@Override
 	public float getKnockbackResistance() {
 		return this.knockbackResistance;
 	}
