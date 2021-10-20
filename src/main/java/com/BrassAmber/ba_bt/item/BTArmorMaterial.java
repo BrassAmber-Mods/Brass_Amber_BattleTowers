93c634836476de61924bb3d3dce3bdd319dd246e
Xratedjunior <marnix@roomer.tv>
2021-10-20 15:05:09 +0200

    Removed BTSwordItem (never used), and moved Item classes to a separate package.

--- src/main/java/com/BrassAmber/ba_bt/item/BTArmorMaterial.java
+++ src/main/java/com/BrassAmber/ba_bt/item/BTArmorMaterial.java
@@ -1,9 +1,11 @@
 package com.BrassAmber.ba_bt.item;
 
+import java.util.function.Supplier;
+
 import com.BrassAmber.ba_bt.BrassAmberBattleTowers;
+
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
-import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.LazyValue;
 import net.minecraft.util.SoundEvent;
@@ -11,64 +13,62 @@ import net.minecraft.util.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-import java.util.function.Supplier;
-
 public enum BTArmorMaterial implements IArmorMaterial {
-   SILVER("silver", 11, new int[]{2, 5, 6, 2}, 20, SoundEvents.ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> {
-      return Ingredient.of(BTItems.SILVER_INGOT);
-   });
+	SILVER("silver", 11, new int[] { 2, 5, 6, 2 }, 20, SoundEvents.ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> {
+		return Ingredient.of(BTItems.SILVER_INGOT);
+	});
 
-   private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
-   private final String name;
-   private final int durabilityMultiplier;
-   private final int[] slotProtections;
-   private final int enchantmentValue;
-   private final SoundEvent sound;
-   private final float toughness;
-   private final float knockbackResistance;
-   private final LazyValue<Ingredient> repairIngredient;
+	private static final int[] HEALTH_PER_SLOT = new int[] { 13, 15, 16, 11 };
+	private final String name;
+	private final int durabilityMultiplier;
+	private final int[] slotProtections;
+	private final int enchantmentValue;
+	private final SoundEvent sound;
+	private final float toughness;
+	private final float knockbackResistance;
+	private final LazyValue<Ingredient> repairIngredient;
 
-   private BTArmorMaterial(String p_i231593_3_, int p_i231593_4_, int[] p_i231593_5_, int p_i231593_6_, SoundEvent p_i231593_7_, float p_i231593_8_, float p_i231593_9_, Supplier<Ingredient> p_i231593_10_) {
-      this.name = p_i231593_3_;
-      this.durabilityMultiplier = p_i231593_4_;
-      this.slotProtections = p_i231593_5_;
-      this.enchantmentValue = p_i231593_6_;
-      this.sound = p_i231593_7_;
-      this.toughness = p_i231593_8_;
-      this.knockbackResistance = p_i231593_9_;
-      this.repairIngredient = new LazyValue<>(p_i231593_10_);
-   }
+	private BTArmorMaterial(String p_i231593_3_, int p_i231593_4_, int[] p_i231593_5_, int p_i231593_6_, SoundEvent p_i231593_7_, float p_i231593_8_, float p_i231593_9_, Supplier<Ingredient> p_i231593_10_) {
+		this.name = p_i231593_3_;
+		this.durabilityMultiplier = p_i231593_4_;
+		this.slotProtections = p_i231593_5_;
+		this.enchantmentValue = p_i231593_6_;
+		this.sound = p_i231593_7_;
+		this.toughness = p_i231593_8_;
+		this.knockbackResistance = p_i231593_9_;
+		this.repairIngredient = new LazyValue<>(p_i231593_10_);
+	}
 
-   public int getDurabilityForSlot(EquipmentSlotType p_200896_1_) {
-      return HEALTH_PER_SLOT[p_200896_1_.getIndex()] * this.durabilityMultiplier;
-   }
+	public int getDurabilityForSlot(EquipmentSlotType p_200896_1_) {
+		return HEALTH_PER_SLOT[p_200896_1_.getIndex()] * this.durabilityMultiplier;
+	}
 
-   public int getDefenseForSlot(EquipmentSlotType p_200902_1_) {
-      return this.slotProtections[p_200902_1_.getIndex()];
-   }
+	public int getDefenseForSlot(EquipmentSlotType p_200902_1_) {
+		return this.slotProtections[p_200902_1_.getIndex()];
+	}
 
-   public int getEnchantmentValue() {
-      return this.enchantmentValue;
-   }
+	public int getEnchantmentValue() {
+		return this.enchantmentValue;
+	}
 
-   public SoundEvent getEquipSound() {
-      return this.sound;
-   }
+	public SoundEvent getEquipSound() {
+		return this.sound;
+	}
 
-   public Ingredient getRepairIngredient() {
-      return this.repairIngredient.get();
-   }
+	public Ingredient getRepairIngredient() {
+		return this.repairIngredient.get();
+	}
 
-   @OnlyIn(Dist.CLIENT)
-   public String getName() {
-      return BrassAmberBattleTowers.MOD_ID + ":" + this.name;
-   }
+	@OnlyIn(Dist.CLIENT)
+	public String getName() {
+		return BrassAmberBattleTowers.MOD_ID + ":" + this.name;
+	}
 
-   public float getToughness() {
-      return this.toughness;
-   }
+	public float getToughness() {
+		return this.toughness;
+	}
 
-   public float getKnockbackResistance() {
-      return this.knockbackResistance;
-   }
+	public float getKnockbackResistance() {
+		return this.knockbackResistance;
+	}
 }
