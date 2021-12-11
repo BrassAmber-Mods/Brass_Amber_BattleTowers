fb45150133b00532dae320da858bc6ec7902dab8
DerToaster98 <seiler.oli@web.de>
2021-12-11 15:49:11 +0100

    own target AI for golems

---
+++ src/main/java/com/BrassAmber/ba_bt/entity/ai/target/TargetTaskGolemLand.java
@@ -0,0 +1,57 @@
+package com.BrassAmber.ba_bt.entity.ai.target;
+
+import com.BrassAmber.ba_bt.entity.hostile.golem.BTGolemEntityAbstract;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class TargetTaskGolemLand<M extends BTGolemEntityAbstract> extends NearestAttackableTargetGoal<PlayerEntity> {
+
+	protected int cooldown = 30;
+	
+	public TargetTaskGolemLand(M p_i50313_1_) {
+		//It does not need to be able to see the target!
+		super(p_i50313_1_, PlayerEntity.class, false);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean canUse() {
+		if(((M)this.mob).getGolemState() == BTGolemEntityAbstract.DORMANT) {
+			this.cooldown--;
+			return this.cooldown <= 0;
+		}
+		return super.canUse();
+	}
+	
+	@Override
+	protected double getFollowDistance() {
+		return 32.0D;
+	}
+	
+	@Override
+	protected boolean canReach(LivingEntity p_75295_1_) {
+		return super.canReach(p_75295_1_);
+	}
+	
+	@Override
+	public void tick() {
+		if(this.target == null) {
+			return;
+		}
+		if(this.mob.distanceTo(this.target) > this.getFollowDistance()) {
+			this.stop();
+			return;
+		}
+		
+		super.tick();
+	}
+	
+	@Override
+	public void stop() {
+		this.cooldown = 100;
+		super.stop();
+	}
+
+}
