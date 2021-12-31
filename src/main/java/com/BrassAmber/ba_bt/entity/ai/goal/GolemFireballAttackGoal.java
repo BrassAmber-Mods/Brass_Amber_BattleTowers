ae9e4dd403daa655e9c293f1287879fa87072e25
JMilamber <stringerj@allegheny.edu>
2021-12-31 00:01:18 -0700

    Fix null playerentity in fireball attack goal crashing the game, added check for if target entity is null

--- src/main/java/com/BrassAmber/ba_bt/entity/ai/goal/GolemFireballAttackGoal.java
+++ src/main/java/com/BrassAmber/ba_bt/entity/ai/goal/GolemFireballAttackGoal.java
@@ -52,63 +52,66 @@ public class GolemFireballAttackGoal extends Goal {
 	 */
 	public void tick() {
 		LivingEntity targetLivingEntity = this.golem.getTarget();
-		// Look at the target.
-		this.golem.getLookControl().setLookAt(targetLivingEntity, 30.0F, 30.0F);
+		// null check for servers/possible respawn targeting bug
+		if (targetLivingEntity != null) {
+			// Look at the target.
+			this.golem.getLookControl().setLookAt(targetLivingEntity, 30.0F, 30.0F);
 
-		// Set the max shooting distance
-		double maxShootingDistance = 64.0D;
-		// We need to square it since we're using that value for calculations.
-		maxShootingDistance *= maxShootingDistance;
+			// Set the max shooting distance
+			double maxShootingDistance = 64.0D;
+			// We need to square it since we're using that value for calculations.
+			maxShootingDistance *= maxShootingDistance;
 
-		// Check if the target is within range and if the Golem is able to see the target.
-		if (targetLivingEntity.distanceToSqr(this.golem) < maxShootingDistance && this.golem.canSee(targetLivingEntity)) {
-			// Increase attack time
-			++this.chargeTime;
+			// Check if the target is within range and if the Golem is able to see the target.
+			if (targetLivingEntity.distanceToSqr(this.golem) < maxShootingDistance && this.golem.canSee(targetLivingEntity)) {
+				// Increase attack time
+				++this.chargeTime;
 
-			// Prepare to shoot. (10 more ticks, or 0.5 seconds)
-			if (this.chargeTime == 10 && !this.golem.isSilent()) {
-				// Play charging sound
-				this.golem.playSoundEventWithVariation(BTSoundEvents.ENTITY_GOLEM_CHARGE);
-			}
-
-			// Shoot fireball
-			if (this.chargeTime >= 20) {
-				// Calculation for fireball trajectory and positioning.
-				Vector3d vector3d = this.golem.getViewVector(1.0F);
-				double xPower = targetLivingEntity.getX() - this.golem.getX();
-				double yPower = targetLivingEntity.getY(0.5D) - (0.5D + this.golem.getY(0.5D));
-				double zPower = targetLivingEntity.getZ() - this.golem.getZ();
-
-				// Get golem world
-				World world = this.golem.level;
-
-				// Play shooting sound
-				if (!this.golem.isSilent()) {
-					world.levelEvent((PlayerEntity) null, 1016, this.golem.blockPosition(), 0);
+				// Prepare to shoot. (10 more ticks, or 0.5 seconds)
+				if (this.chargeTime == 10 && !this.golem.isSilent()) {
+					// Play charging sound
+					this.golem.playSoundEventWithVariation(BTSoundEvents.ENTITY_GOLEM_CHARGE);
 				}
 
-				// Create fireball
-				DamagingProjectileEntity fireballentity = this.createFireBall(world, xPower, yPower, zPower);
-				// Set fireball initial position
-				double lateralSpawnPositionOffset = 1.2D;
-				double verticalSpawnPositionOffset = 0.5D;
-				fireballentity.setPos(this.golem.getX() + vector3d.x * lateralSpawnPositionOffset, this.golem.getY(0.5D) + verticalSpawnPositionOffset, fireballentity.getZ() + vector3d.z * lateralSpawnPositionOffset);
-				// Add fireball to the world
-				world.addFreshEntity(fireballentity);
+				// Shoot fireball
+				if (this.chargeTime >= 20) {
+					// Calculation for fireball trajectory and positioning.
+					Vector3d vector3d = this.golem.getViewVector(1.0F);
+					double xPower = targetLivingEntity.getX() - this.golem.getX();
+					double yPower = targetLivingEntity.getY(0.5D) - (0.5D + this.golem.getY(0.5D));
+					double zPower = targetLivingEntity.getZ() - this.golem.getZ();
 
-				// set firing timeout between shoots
-				this.chargeTime = this.golem.isEnraged() ? -20 : -40;
+					// Get golem world
+					World world = this.golem.level;
+
+					// Play shooting sound
+					if (!this.golem.isSilent()) {
+						world.levelEvent((PlayerEntity) null, 1016, this.golem.blockPosition(), 0);
+					}
+
+					// Create fireball
+					DamagingProjectileEntity fireballentity = this.createFireBall(world, xPower, yPower, zPower);
+					// Set fireball initial position
+					double lateralSpawnPositionOffset = 1.2D;
+					double verticalSpawnPositionOffset = 0.5D;
+					fireballentity.setPos(this.golem.getX() + vector3d.x * lateralSpawnPositionOffset, this.golem.getY(0.5D) + verticalSpawnPositionOffset, fireballentity.getZ() + vector3d.z * lateralSpawnPositionOffset);
+					// Add fireball to the world
+					world.addFreshEntity(fireballentity);
+
+					// set firing timeout between shoots
+					this.chargeTime = this.golem.isEnraged() ? -20 : -40;
+				}
 			}
-		}
 
-		// If target is too far away or the golem can't see the target and the current charge time is more than 0.
-		else if (this.chargeTime > 0) {
-			// Decrease charge time until 0. (Which is the default)
-			--this.chargeTime;
-		}
+			// If target is too far away or the golem can't see the target and the current charge time is more than 0.
+			else if (this.chargeTime > 0) {
+				// Decrease charge time until 0. (Which is the default)
+				--this.chargeTime;
+			}
 
-		// 10 ticks before shooting, so while preparing to shoot, set the DataParameter charging to 'true'.
-		this.golem.setCharging(this.chargeTime > 10);
+			// 10 ticks before shooting, so while preparing to shoot, set the DataParameter charging to 'true'.
+			this.golem.setCharging(this.chargeTime > 10);
+		}
 	}
 	
 	protected DamagingProjectileEntity createFireBall(World world, double xPower, double yPower, double zPower) {
