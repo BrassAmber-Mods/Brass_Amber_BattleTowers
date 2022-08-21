964bdab414282e72fa90f3e83eb30e1967f97daf
JMilamber <stringerj@allegheny.edu>
2022-08-21 14:37:53 -0600

    Change Resonance stone to use 'finishUsingItem' instead of 'onUse' to activate/deactivate effects.

--- src/main/java/com/BrassAmber/ba_bt/item/item/ResonanceStoneItem.java
+++ src/main/java/com/BrassAmber/ba_bt/item/item/ResonanceStoneItem.java
@@ -1,38 +1,23 @@
 package com.BrassAmber.ba_bt.item.item;
 
 import com.BrassAmber.ba_bt.entity.block.BTAbstractObelisk;
-import com.BrassAmber.ba_bt.init.BTBlocks;
 import com.BrassAmber.ba_bt.init.BTEntityTypes;
 import com.BrassAmber.ba_bt.init.BTExtras;
-import com.BrassAmber.ba_bt.sound.BTSoundEvents;
 import com.BrassAmber.ba_bt.util.BTUtil;
 import com.BrassAmber.ba_bt.util.GolemType;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.RecordItem;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class ResonanceStoneItem extends RecordItem {
 
@@ -54,13 +39,15 @@ public class ResonanceStoneItem extends RecordItem {
 
     @Override
     public int getUseDuration(ItemStack itemStack) {
-        return 1;
+        return 20;
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        this.effectOn = !this.effectOn;
-        return InteractionResultHolder.success(player.getItemInHand(hand));
+    public @NotNull ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity entity) {
+        if (itemStack.isEnchanted()) {
+            this.effectOn = !this.effectOn;
+        }
+        return super.finishUsingItem(itemStack, level, entity);
     }
 
     public void addEnchantment(ItemStack stackInUse) {
@@ -91,7 +78,7 @@ public class ResonanceStoneItem extends RecordItem {
 
         if (this.enchantment != null && EnchantmentHelper.getEnchantments(itemStack).containsKey(this.enchantment)) {
             if (entity instanceof LivingEntity living && this.effectOn) {
-                living.forceAddEffect(new MobEffectInstance(BTExtras.DEPTH_DROPPER_EFFECT.get(),200, 3), living);
+                living.addEffect(new MobEffectInstance(BTExtras.DEPTH_DROPPER_EFFECT.get(),200, 3), living);
             }
         }
     }
