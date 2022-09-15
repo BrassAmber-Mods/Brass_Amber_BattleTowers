910958ba88d8c7fe5eb86848cd7ff2ca176ce304
JMilamber <stringerj@allegheny.edu>
2022-09-15 01:34:39 -0600

    New holder items for Loot generation.

---
+++ src/main/java/com/BrassAmber/ba_bt/item/item/BTEmptyLootItem.java
@@ -0,0 +1,9 @@
+package com.BrassAmber.ba_bt.item.item;
+
+import net.minecraft.world.item.Item;
+
+public class BTEmptyLootItem extends Item {
+    public BTEmptyLootItem(Properties p_41383_) {
+        super(p_41383_);
+    }
+}
