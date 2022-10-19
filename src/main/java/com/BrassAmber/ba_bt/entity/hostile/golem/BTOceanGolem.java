3c52218b9207c697ac4dbdce1f82b9b7b8278170
JMilamber <stringerj@allegheny.edu>
2022-10-19 01:56:03 -0600

    Tweak roar & blindness combo to trigger less often. Attempt to fix speed bug of golem. (possibly due to charge attack?)

--- src/main/java/com/BrassAmber/ba_bt/entity/hostile/golem/BTOceanGolem.java
+++ src/main/java/com/BrassAmber/ba_bt/entity/hostile/golem/BTOceanGolem.java
@@ -1,21 +1,11 @@
 package com.BrassAmber.ba_bt.entity.hostile.golem;
 
-import com.BrassAmber.ba_bt.entity.ai.goal.GolemFireballAttackGoal;
-import com.BrassAmber.ba_bt.entity.ai.goal.GolemLeapGoal;
-import com.BrassAmber.ba_bt.entity.ai.goal.GolemStompAttackGoal;
 import com.BrassAmber.ba_bt.entity.ai.goal.oceangolem.DashAttackGoal;
-import com.BrassAmber.ba_bt.init.BTExtras;
 import com.BrassAmber.ba_bt.util.GolemType;
-import net.minecraft.client.particle.WaterCurrentDownParticle;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.util.Mth;
 import net.minecraft.world.BossEvent;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -23,19 +13,10 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.control.LookControl;
-import net.minecraft.world.entity.ai.control.MoveControl;
+import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl;
 import net.minecraft.world.entity.ai.control.SmoothSwimmingMoveControl;
-import net.minecraft.world.entity.ai.goal.RandomSwimmingGoal;
-import net.minecraft.world.entity.animal.Pufferfish;
-import net.minecraft.world.entity.monster.Guardian;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BubbleColumnBlock;
-import net.minecraft.world.level.block.SoulSandBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.fluids.FluidAttributes;
 
 import java.util.List;
 
@@ -47,7 +28,8 @@ public class BTOceanGolem extends BTAbstractGolem {
 
 	public BTOceanGolem(EntityType<? extends BTOceanGolem> type, Level levelIn) {
 		super(type, levelIn, BossEvent.BossBarColor.YELLOW);
-		this.moveControl = new SmoothSwimmingMoveControl(this, 85, 10, 1F, 1F, true);
+		this.moveControl = new SmoothSwimmingMoveControl(this, 65, 10, .8F, .8F, true);
+		this.lookControl = new SmoothSwimmingLookControl(this, 10);
 		this.setGolemName(GolemType.OCEAN.getDisplayName());
 		this.setBossBarName();
 		// Sets the experience points to drop. Reference taken from the EnderDragon.
@@ -57,7 +39,7 @@ public class BTOceanGolem extends BTAbstractGolem {
 	}
 
 	public static AttributeSupplier.Builder createBattleGolemAttributes() {
-		return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, oceanGolemHP.get()).add(Attributes.MOVEMENT_SPEED, 1.7D).add(Attributes.KNOCKBACK_RESISTANCE, 2.0D).add(Attributes.ATTACK_DAMAGE, 15.0D).add(Attributes.FOLLOW_RANGE, 60.0D).add(Attributes.ARMOR, 4);
+		return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, oceanGolemHP.get()).add(Attributes.MOVEMENT_SPEED, .2D).add(Attributes.KNOCKBACK_RESISTANCE, 2.0D).add(Attributes.ATTACK_DAMAGE, 15.0D).add(Attributes.FOLLOW_RANGE, 60.0D).add(Attributes.ARMOR, 4);
 	}
 
 	public void find_golem_chest(BlockPos spawnPos) {
@@ -72,9 +54,10 @@ public class BTOceanGolem extends BTAbstractGolem {
 		super.tick();
 		if (this.getTarget() != null && !this.level.isClientSide()) {
 			LivingEntity target = this.getTarget();
-			if (distanceTo(target) > 16 && this.random.nextInt(50) == 1) {
+			if (distanceTo(target) > 16 && this.random.nextInt(500) == 1) {
 				this.playRoarSound();
-				target.addEffect(new MobEffectInstance(MobEffects.BLINDNESS,40, 1), target);
+				target.addEffect(new MobEffectInstance(MobEffects.BLINDNESS,60, 1), target);
+				this.playRoarSound();
 			}
 		}
 	}
@@ -85,8 +68,7 @@ public class BTOceanGolem extends BTAbstractGolem {
 
 	@Override
 	protected void addBehaviorGoals() {
-		super.addBehaviorGoals();
-		this.goalSelector.addGoal(7, new DashAttackGoal(this, 10));
+		this.goalSelector.addGoal(8, new DashAttackGoal(this, 10));
 	}
 
 
@@ -140,7 +122,7 @@ public class BTOceanGolem extends BTAbstractGolem {
 
 
 	protected void playRoarSound() {
-		this.playSoundEvent(SoundEvents.ENDER_DRAGON_GROWL, 1.2F);
+		this.playSoundEvent(SoundEvents.ENDER_DRAGON_GROWL, 1.6F);
 	}
 
 }
