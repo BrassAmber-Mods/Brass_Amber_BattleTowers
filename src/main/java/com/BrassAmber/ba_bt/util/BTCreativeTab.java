192003b663be2aacc09de3ae7646e8d026ffd4b6
Milamber <socky79.turbulance@gmail.com>
2023-06-19 00:42:55 -0600

    Fix CreativeModeTab Creation

--- src/main/java/com/BrassAmber/ba_bt/util/BTCreativeTab.java
+++ src/main/java/com/BrassAmber/ba_bt/util/BTCreativeTab.java
@@ -2,19 +2,20 @@ package com.BrassAmber.ba_bt.util;
 
 import com.BrassAmber.ba_bt.BrassAmberBattleTowers;
 import com.BrassAmber.ba_bt.init.BTBlocks;
-
-
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
-public class BTCreativeTab extends CreativeModeTab {
+public class BTCreativeTab {
 
-	public BTCreativeTab() {
-		super(BrassAmberBattleTowers.MOD_ID);
-	}
+	public static DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB.registry(), BrassAmberBattleTowers.MOD_ID);
 
-	@Override
-	public ItemStack makeIcon() {
-		return new ItemStack(BTBlocks.TAB_ICON.get());
-	}
+	public static RegistryObject<CreativeModeTab> BT_TAB = CREATIVE_MODE_TABS.register("bt_tab", () ->
+			CreativeModeTab.builder().icon(() -> new ItemStack(BTBlocks.TAB_ICON.get()))
+					.title(Component.translatable("creativemodetab.bt_tab")).build());
+
 }
