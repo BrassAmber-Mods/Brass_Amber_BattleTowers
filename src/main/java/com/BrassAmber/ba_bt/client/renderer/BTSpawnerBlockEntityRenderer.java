c417cc58b364df219157f1f1cc1bc6355290e48f
Milamber <socky79.turbulance@gmail.com>
2023-07-17 22:28:53 -0600

    update comparable to SpawnerRenderer.java

--- src/main/java/com/BrassAmber/ba_bt/client/renderer/BTSpawnerBlockEntityRenderer.java
+++ src/main/java/com/BrassAmber/ba_bt/client/renderer/BTSpawnerBlockEntityRenderer.java
@@ -2,11 +2,11 @@ package com.BrassAmber.ba_bt.client.renderer;
 
 import com.BrassAmber.ba_bt.block.blockentity.spawner.BTAbstractSpawnerBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Vector3f;
-import net.minecraft.client.Minecraft;
+import com.mojang.math.Axis;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BaseSpawner;
@@ -15,14 +15,16 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class BTSpawnerBlockEntityRenderer implements BlockEntityRenderer<BTAbstractSpawnerBlockEntity> {
-    public BTSpawnerBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
+    private final EntityRenderDispatcher entityRenderer;
+    public BTSpawnerBlockEntityRenderer(BlockEntityRendererProvider.Context p_173673_) {
+        this.entityRenderer = p_173673_.getEntityRenderer();
     }
 
     public void render(BTAbstractSpawnerBlockEntity p_112563_, float p_112564_, PoseStack p_112565_, MultiBufferSource p_112566_, int p_112567_, int p_112568_) {
         p_112565_.pushPose();
         p_112565_.translate(0.5D, 0.0D, 0.5D);
         BaseSpawner basespawner = p_112563_.getSpawner();
-        Entity entity = basespawner.getOrCreateDisplayEntity(p_112563_.getLevel());
+        Entity entity = basespawner.getOrCreateDisplayEntity(p_112563_.getLevel(), p_112563_.getLevel().getRandom(), p_112563_.getBlockPos());
         if (entity != null) {
             float f = 0.53125F;
             float f1 = Math.max(entity.getBbWidth(), entity.getBbHeight());
@@ -31,11 +33,11 @@ public class BTSpawnerBlockEntityRenderer implements BlockEntityRenderer<BTAbstr
             }
 
             p_112565_.translate(0.0D, (double)0.4F, 0.0D);
-            p_112565_.mulPose(Vector3f.YP.rotationDegrees((float) Mth.lerp((double)p_112564_, basespawner.getoSpin(), basespawner.getSpin()) * 10.0F));
+            p_112565_.mulPose(Axis.YP.rotationDegrees((float) Mth.lerp((double)p_112564_, basespawner.getoSpin(), basespawner.getSpin()) * 10.0F));
             p_112565_.translate(0.0D, (double)-0.2F, 0.0D);
-            p_112565_.mulPose(Vector3f.XP.rotationDegrees(-30.0F));
+            p_112565_.mulPose(Axis.XP.rotationDegrees(-30.0F));
             p_112565_.scale(f, f, f);
-            Minecraft.getInstance().getEntityRenderDispatcher().render(entity, 0.0D, 0.0D, 0.0D, 0.0F, p_112564_, p_112565_, p_112566_, p_112567_);
+            this.entityRenderer.render(entity, 0.0D, 0.0D, 0.0D, 0.0F, p_112564_, p_112565_, p_112566_, p_112567_);
         }
 
         p_112565_.popPose();
