cd0832274039cd8fede2e71dd1014c0f32a38761
Milamber <socky79.turbulance@gmail.com>
2023-07-22 22:51:47 -0600

    Update chestblockitem

--- src/main/java/com/BrassAmber/ba_bt/item/ChestBlockItem.java
+++ src/main/java/com/BrassAmber/ba_bt/item/ChestBlockItem.java
@@ -2,34 +2,26 @@ package com.BrassAmber.ba_bt.item;
 
 import com.BrassAmber.ba_bt.BrassAmberBattleTowers;
 import com.BrassAmber.ba_bt.block.block.TowerChestBlock;
-import com.BrassAmber.ba_bt.block.blockentity.inventory.BTChestItemRenderer;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.client.IItemRenderProperties;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.function.Consumer;
+
 
 public class ChestBlockItem extends BlockItem {
     public ChestBlockItem(Block block, Properties properties) {
         super(block, properties);
     }
 
-    @Override
-    public void initializeClient(Consumer<?> consumer) {
-        super.initializeClient(consumer);
-        consumer.accept(ChestProperty.INSTANCE);
-    }
-
+    
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
 
         if (Screen.hasShiftDown()) {
@@ -44,11 +36,3 @@ public class ChestBlockItem extends BlockItem {
     }
 }
 
-class ChestProperty implements IItemRenderProperties {
-    public static ChestProperty INSTANCE = new ChestProperty();
-
-    @Override
-    public BlockEntityWithoutLevelRenderer getItemStackRenderer() {
-        return BTChestItemRenderer.INSTANCE;
-    }
-}
