f1c8cb6670d7c3b19f3f58eeeb37972e72897920
Milamber <socky79.turbulance@gmail.com>
2023-08-21 22:42:59 -0600

    update BTStructures class

--- src/main/java/com/BrassAmber/ba_bt/init/BTStructures.java
+++ src/main/java/com/BrassAmber/ba_bt/init/BTStructures.java
@@ -1,19 +1,25 @@
 package com.BrassAmber.ba_bt.init;
 
 import com.BrassAmber.ba_bt.BrassAmberBattleTowers;
+import com.BrassAmber.ba_bt.worldGen.structures.BattleTowerStructure;
 import com.BrassAmber.ba_bt.worldGen.structures.LandBattleTower;
 import com.BrassAmber.ba_bt.worldGen.structures.OceanBattleTower;
-import net.minecraft.world.level.StructureFeatureManager;
-import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class BTStructures {
 
-    public static final DeferredRegister<StructureFeature<?>> STRUCTURE_REGISTRY = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, BrassAmberBattleTowers.MOD_ID);
+    public static final DeferredRegister<StructureType<?>> STRUCTURE_REGISTRY = DeferredRegister.create(Registries.STRUCTURE_TYPE, BrassAmberBattleTowers.MOD_ID);
 
-    public static final RegistryObject<StructureFeature<?>> LAND_BATTLE_TOWER = STRUCTURE_REGISTRY.register("bt_land_tower", LandBattleTower::new);
-    public static final RegistryObject<StructureFeature<?>> OCEAN_BATTLE_TOWER = STRUCTURE_REGISTRY.register("bt_ocean_tower", OceanBattleTower::new);
+    public static final RegistryObject<StructureType<LandBattleTower>> LAND_BATTLE_TOWER = STRUCTURE_REGISTRY.register("bt_land_tower", () -> explicitStructureTypeTyping(LandBattleTower.CODEC));
+    public static final RegistryObject<StructureType<BattleTowerStructure>> OCEAN_BATTLE_TOWER = STRUCTURE_REGISTRY.register("bt_ocean_tower", () -> explicitStructureTypeTyping(OceanBattleTower.CODEC));
 
+
+    private static <T extends Structure> StructureType<T> explicitStructureTypeTyping(Codec<T> structureCodec) {
+        return () -> structureCodec;
+    }
 }
