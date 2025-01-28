a29f88c28b5646c99f067f4f7e7f7dfabd13fa09
JMilamber <socky79.turbulance@gmail.com>
2025-01-28 12:49:48 -0500

    remove unneeded overrides and update chests to actually have 36 slots.

--- src/main/java/com/brass_amber/ba_bt/block/blockentity/GolemChestBlockEntity.java
+++ src/main/java/com/brass_amber/ba_bt/block/blockentity/GolemChestBlockEntity.java
@@ -7,17 +7,16 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.CompoundContainer;
 import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.LockCode;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.entity.*;
 import net.minecraft.world.level.block.state.BlockState;
@@ -32,32 +31,7 @@ public class GolemChestBlockEntity extends ChestBlockEntity {
 	private LockCode lockKey = new LockCode("bt_spawner");
 	protected boolean unlocked = false;
 
-	private static final int EVENT_SET_OPEN_COUNT = 1;
 	private NonNullList<ItemStack> items = NonNullList.withSize(36, ItemStack.EMPTY);
-	private final ContainerOpenersCounter openersCounter = new ContainerOpenersCounter() {
-		protected void onOpen(Level level, BlockPos blockPos, BlockState blockState) {
-			GolemChestBlockEntity.playSound(level, blockPos, blockState, SoundEvents.CHEST_OPEN);
-		}
-
-		protected void onClose(Level level, BlockPos blockPos, BlockState blockState) {
-			GolemChestBlockEntity.playSound(level, blockPos, blockState, SoundEvents.CHEST_CLOSE);
-		}
-
-		protected void openerCountChanged(Level level, BlockPos blockPos, BlockState blockState, int i, int i1) {
-			GolemChestBlockEntity.this.signalOpenCount(level, blockPos, blockState, i, i1);
-		}
-
-		protected boolean isOwnContainer(Player player) {
-			if (!(player.containerMenu instanceof ChestMenu)) {
-				return false;
-			} else {
-				Container container = ((ChestMenu) player.containerMenu).getContainer();
-				return container == GolemChestBlockEntity.this || container instanceof CompoundContainer && ((CompoundContainer)container).contains(GolemChestBlockEntity.this);
-			}
-		}
-	};
-
-	private final ChestLidController chestLidController = new ChestLidController();
 
 	protected GolemChestBlockEntity(BlockEntityType<? extends ChestBlockEntity> blockEntityType, BlockPos blockPos, BlockState blockState) {
 		super(blockEntityType, blockPos, blockState);
@@ -68,8 +42,14 @@ public class GolemChestBlockEntity extends ChestBlockEntity {
 		this(getChestEntity(blockState.getBlock()), blockPos, blockState);
 	}
 
+	@Override
 	public int getContainerSize() {
 		return 36;
+	}
+
+	@Override
+	protected AbstractContainerMenu createMenu(int p_59082_, Inventory p_59083_) {
+		return ChestMenu.fourRows(p_59082_, p_59083_);
 	}
 
 	/**
@@ -81,6 +61,7 @@ public class GolemChestBlockEntity extends ChestBlockEntity {
 		return Component.translatable("container.ba_bt." + this.tower_name + "_golem_chest");
 	}
 
+	@Override
 	public void load(CompoundTag compoundTag) {
 		super.load(compoundTag);
 		this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
@@ -91,28 +72,13 @@ public class GolemChestBlockEntity extends ChestBlockEntity {
 
 	}
 
+	@Override
 	protected void saveAdditional(CompoundTag compoundTag) {
 		super.saveAdditional(compoundTag);
 		if (!this.trySaveLootTable(compoundTag)) {
 			ContainerHelper.saveAllItems(compoundTag, this.items);
 		}
 		compoundTag.putBoolean("Unlocked", this.unlocked);
-	}
-
-	static void playSound(Level p_155339_, BlockPos p_155340_, BlockState p_155341_, SoundEvent p_155342_) {
-		ChestType chesttype = p_155341_.getValue(ChestBlock.TYPE);
-		if (chesttype != ChestType.LEFT) {
-			double d0 = (double)p_155340_.getX() + 0.5D;
-			double d1 = (double)p_155340_.getY() + 0.5D;
-			double d2 = (double)p_155340_.getZ() + 0.5D;
-			if (chesttype == ChestType.RIGHT) {
-				Direction direction = ChestBlock.getConnectedDirection(p_155341_);
-				d0 += (double)direction.getStepX() * 0.5D;
-				d2 += (double)direction.getStepZ() * 0.5D;
-			}
-
-			p_155339_.playSound((Player)null, d0, d1, d2, p_155342_, SoundSource.BLOCKS, 0.5F, p_155339_.random.nextFloat() * 0.1F + 0.9F);
-		}
 	}
 
 	private net.minecraftforge.items.IItemHandlerModifiable createHandler() {
@@ -174,21 +140,10 @@ public class GolemChestBlockEntity extends ChestBlockEntity {
 		return this.worldPosition;
 	}
 
-	@Override
-	public float getOpenNess(float p_59604_) {
-		return 0;
-	}
-
-	@Override
-	public NonNullList<ItemStack> getItems() {
-		return items;
-	}
 
 	@Override
 	public void setItems(@NotNull NonNullList<ItemStack> items) {
-		for (int i = 0; i < items.size(); i++) {
-			this.items.set(i, items.get(i));
-		}
+		super.setItems(items);
 	}
 }
 
