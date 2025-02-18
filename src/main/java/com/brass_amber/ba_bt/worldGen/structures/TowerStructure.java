2b8fb07e50101afb9ff5f37a985aa86e3ba21efc
JMilamber <socky79.turbulance@gmail.com>
2025-02-17 22:16:04 -0500

    override base functions to prevent /locate failing and remove excess biome files

--- src/main/java/com/brass_amber/ba_bt/worldGen/structures/TowerStructure.java
+++ src/main/java/com/brass_amber/ba_bt/worldGen/structures/TowerStructure.java
@@ -3,6 +3,7 @@ package com.brass_amber.ba_bt.worldGen.structures;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import com.brass_amber.ba_bt.BABTMain;
 import com.brass_amber.ba_bt.BattleTowersConfig;
@@ -15,15 +16,21 @@ import net.minecraft.core.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.biome.Biome;
 
+import net.minecraft.world.level.biome.BiomeSource;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.structure.*;
 import net.minecraft.world.level.levelgen.structure.pieces.PiecesContainer;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
+import org.jetbrains.annotations.NotNull;
 
 import static com.brass_amber.ba_bt.BABTMain.SAVE_TOWERS;
 import static com.brass_amber.ba_bt.BattleTowersConfig.*;
@@ -52,16 +59,50 @@ public abstract class TowerStructure extends Structure {
     }
 
     public static <S extends TowerStructure> RecordCodecBuilder<S, TowerStructure.BTStructureSettings> extraSettingsCodec() {
-        return TowerStructure.BTStructureSettings.CODEC.forGetter((object) -> new BTStructureSettings(null, null, null, null));
+        return TowerStructure.BTStructureSettings.CODEC.forGetter((object) -> new BTStructureSettings(null));
     }
 
     @Override
-    public void afterPlace(WorldGenLevel p_226560_, StructureManager p_226561_, ChunkGenerator p_226562_, RandomSource p_226563_, BoundingBox p_226564_, ChunkPos p_226565_, PiecesContainer p_226566_) {
-        super.afterPlace(p_226560_, p_226561_, p_226562_, p_226563_, p_226564_, p_226565_, p_226566_);
+    public void afterPlace(WorldGenLevel worldGenLevel, StructureManager structureManager, ChunkGenerator chunkGenerator, RandomSource randomSource, BoundingBox boundingBox, ChunkPos chunkPos, PiecesContainer piecesContainer) {
+        super.afterPlace(worldGenLevel, structureManager, chunkGenerator, randomSource, boundingBox, chunkPos, piecesContainer);
     }
 
     @Override
-    protected Optional<Structure.GenerationStub> findGenerationPoint(GenerationContext generationContext) {
+    public @NotNull StructureStart generate(RegistryAccess registryAccess, ChunkGenerator chunkGenerator, BiomeSource biomeSource, RandomState randomState, StructureTemplateManager templateManager, long seed, ChunkPos chunkPos, int i, LevelHeightAccessor heightAccessor, Predicate<Holder<Biome>> biomePredicate) {
+        Structure.GenerationContext structure$generationcontext = new Structure.GenerationContext(registryAccess, chunkGenerator, biomeSource, randomState, templateManager, seed, chunkPos, heightAccessor, biomePredicate);
+        Optional<Structure.GenerationStub> optional = this.findGenerationPoint(structure$generationcontext);
+        if (optional.isPresent()) {
+            StructurePiecesBuilder structurepiecesbuilder = optional.get().getPiecesBuilder();
+            StructureStart structurestart = new StructureStart(this, new ChunkPos(optional.get().position()), i, structurepiecesbuilder.build());
+            if (structurestart.isValid()) {
+                return structurestart;
+            }
+        }
+
+        return StructureStart.INVALID_START;
+    }
+
+    @Override
+    // Override findValidGeneration point as well as findGenerationPoint is called during locate command.
+    // Override of generate above removes this being called anywhere except when locating the tower after chunkGen
+    public @NotNull Optional<GenerationStub> findValidGenerationPoint(GenerationContext generationContext) {
+        boolean canSpawn = false;
+        ChunkPos checkPos = generationContext.chunkPos();
+        for (ChunkPos towerPos : SaveTowers.towers.get(this.towerId)) {
+            if (towerPos.x == checkPos.x && towerPos.z == checkPos.z) {
+                canSpawn = true;
+                break;
+            }
+        }
+
+        if (canSpawn) {
+            return Optional.of(new Structure.GenerationStub(checkPos.getWorldPosition(), (structurePiecesBuilder) -> new StructurePiecesBuilder()));
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    protected @NotNull Optional<Structure.GenerationStub> findGenerationPoint(GenerationContext generationContext) {
 
         ChunkPos chunkPos = generationContext.chunkPos();
         // BABTMain.LOGGER.info("Attempting Land Tower Spawn at " + chunkPos.x + " " + chunkPos.z);
@@ -71,7 +112,6 @@ public abstract class TowerStructure extends Structure {
             return Optional.empty();
         }
 
-        ChunkGenerator chunkGen = generationContext.chunkGenerator();
         int minimumSeparation = this.minimumSeperations.get(this.towerId);
         int seperationRange = this.averageSeperations.get(this.towerId);
 
@@ -119,43 +159,18 @@ public abstract class TowerStructure extends Structure {
 
     protected abstract Pair<Boolean, BlockPos> isSpawnableChunk(GenerationContext generationContext);
 
-    public record BTStructureSettings(HolderSet<Structure> avoidStructures, HolderSet<Biome> biomesTerra,
-                                      HolderSet<Biome> biomesBOP, HolderSet<Biome> biomesBYG) {
+    public record BTStructureSettings(HolderSet<Structure> avoidStructures) {
         public static final MapCodec<TowerStructure.BTStructureSettings> CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(
-                RegistryCodecs.homogeneousList(Registries.STRUCTURE).fieldOf("avoid_structures").forGetter(btStructureSettings -> btStructureSettings.avoidStructures),
-                RegistryCodecs.homogeneousList(Registries.BIOME).fieldOf("biomes_terra").forGetter(structure -> structure.biomesTerra),
-                RegistryCodecs.homogeneousList(Registries.BIOME).fieldOf("biomes_bop").forGetter(structure -> structure.biomesBOP),
-                RegistryCodecs.homogeneousList(Registries.BIOME).fieldOf("biomes_byg").forGetter(structure -> structure.biomesBYG)
+                RegistryCodecs.homogeneousList(Registries.STRUCTURE).fieldOf("avoid_structures").forGetter(btStructureSettings -> btStructureSettings.avoidStructures)
         ).apply(instance, BTStructureSettings::new));
     }
     // Used for tower saving and logging of tower positions
     public void saveTower(BlockPos spawnPos, ChunkPos chunkPos) {
-        BABTMain.LOGGER.info(this.towerName + " Tower at " + spawnPos + " " + chunkPos);
+        BABTMain.LOGGER.info("{} Tower at {} {}", this.towerName, spawnPos, chunkPos);
         SAVE_TOWERS.addTower(chunkPos, this.towerName);
     }
 
-    public boolean acceptableBiome(StructureSettings settings, BTStructureSettings extraSettings, Holder<Biome> biome) {
-        boolean acceptable = false;
-
-        // Test base minecraft acceptable biomes
-        acceptable = settings.biomes().contains(biome);
-
-        // Test Terralith Mod acceptable biomes
-        if (terralithBiomeSpawning.get()) {
-            acceptable = extraSettings.biomesTerra().contains(biome);
-        }
-
-        // Test Biomes Of Plenty Mod acceptable biomes
-        if (biomesOfPlentyBiomeSpawning.get()) {
-            acceptable = extraSettings.biomesBOP().contains(biome);
-        }
-
-        if (biomesYoullGoBiomeSpawning.get()) {
-            acceptable = extraSettings.biomesBYG().contains(biome);
-        }
-
-        return acceptable;
-    }
+    protected abstract boolean isValidBiome(Structure.GenerationStub generationStub, Structure.GenerationContext generationContext, Block topBlock);
 }
 
 
