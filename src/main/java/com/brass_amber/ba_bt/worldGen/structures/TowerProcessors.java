77279157bda89485bf9adec7c51a3e2209934488
JMilamber <socky79.turbulance@gmail.com>
2025-03-11 13:21:46 -0400

    Add OceanTower processors

--- src/main/java/com/brass_amber/ba_bt/worldGen/structures/TowerProcessors.java
+++ src/main/java/com/brass_amber/ba_bt/worldGen/structures/TowerProcessors.java
@@ -1,12 +1,16 @@
 package com.brass_amber.ba_bt.worldGen.structures;
 
 import com.google.common.collect.ImmutableList;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CrossCollisionBlock;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.level.levelgen.structure.templatesystem.*;
 
 import java.util.List;
+import java.util.Random;
 
 import static com.brass_amber.ba_bt.util.BTTags.Blocks.BASE_PROTECTED_TAG;
 
@@ -20,7 +24,16 @@ public class TowerProcessors {
 
     static final StructureProcessor CARPET_PLACER;
 
+    static final StructureProcessor NORMAL_OCEAN;
+    static final StructureProcessor NORMAL_FLOOR_OCEAN;
+    static final StructureProcessor NORMAL_STAIRS_OCEAN;
+
+    static final StructureProcessor BONE_REMOVE;
+    static final StructureProcessor WATERLOGGED;
+
     static {
+
+        Random random = new Random();
 
         BASE_PROTECTED = new ProtectedBlockProcessor(BASE_PROTECTED_TAG);
 
@@ -134,6 +147,7 @@ public class TowerProcessors {
                         Blocks.POLISHED_GRANITE.defaultBlockState()
                 )
         ));
+
         NORMAL_FLOOR_LAND = new RuleProcessor(ImmutableList.of(
                 new ProcessorRule(
                         new RandomBlockMatchTest(Blocks.END_STONE_BRICKS, 0.33F),
@@ -161,6 +175,8 @@ public class TowerProcessors {
                         Blocks.STONE_BRICKS.defaultBlockState()
                 )
         ));
+
+
         CARPET_PLACER = new RuleProcessor(ImmutableList.of(
                 new ProcessorRule(
                         new BlockMatchTest(Blocks.RED_WOOL),
@@ -174,5 +190,110 @@ public class TowerProcessors {
                 )
 
         ));
+
+
+        NORMAL_OCEAN = new RuleProcessor(ImmutableList.of(
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.DARK_PRISMARINE, 0.29F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE.defaultBlockState()
+                )
+
+        ));
+
+        NORMAL_STAIRS_OCEAN = new RuleProcessor(ImmutableList.of(
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.PURPUR_SLAB, 0.2F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE_SLAB.defaultBlockState()
+                ),
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.PURPUR_SLAB, 0.3F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE_BRICK_SLAB.defaultBlockState()
+                ),
+                new ProcessorRule(
+                        new BlockMatchTest(Blocks.PURPUR_SLAB),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.DARK_PRISMARINE_SLAB.defaultBlockState()
+                ),
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.END_STONE_BRICK_SLAB, 0.2F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE_SLAB.defaultBlockState().setValue(SlabBlock.TYPE, SlabType.TOP)
+                ),
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.END_STONE_BRICK_SLAB, 0.3F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE_BRICK_SLAB.defaultBlockState().setValue(SlabBlock.TYPE, SlabType.TOP)
+                ),
+                new ProcessorRule(
+                        new BlockMatchTest(Blocks.END_STONE_BRICK_SLAB),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.DARK_PRISMARINE_SLAB.defaultBlockState().setValue(SlabBlock.TYPE, SlabType.TOP)
+                ),
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.END_STONE_BRICKS, 0.2F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.DARK_PRISMARINE.defaultBlockState()
+                ),
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.END_STONE_BRICKS, 0.1F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE.defaultBlockState()
+                ),
+                new ProcessorRule(
+                        new BlockMatchTest(Blocks.END_STONE_BRICKS),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE_BRICKS.defaultBlockState()
+                )
+        ));
+
+        NORMAL_FLOOR_OCEAN = new RuleProcessor(ImmutableList.of(
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.PURPUR_BLOCK, 0.2F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE.defaultBlockState()
+                ),
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.PURPUR_BLOCK, 0.05F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.MAGMA_BLOCK.defaultBlockState()
+                ),
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.PURPUR_BLOCK, 0.05F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.SOUL_SAND.defaultBlockState()
+                ),
+
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.PURPUR_BLOCK, 0.15F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.WARPED_STEM.defaultBlockState().setValue(RotatedPillarBlock.AXIS, random.nextFloat() > .5 ? Direction.Axis.Z : Direction.Axis.X )
+                ),
+                new ProcessorRule(
+                        new BlockMatchTest(Blocks.PURPUR_BLOCK),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.PRISMARINE_BRICKS.defaultBlockState()
+                )
+        ));
+
+        BONE_REMOVE = new RuleProcessor(ImmutableList.of(
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.BONE_BLOCK, 0.2F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.WATER.defaultBlockState()
+                )
+        ));
+
+        WATERLOGGED = new RuleProcessor(ImmutableList.of(
+                new ProcessorRule(
+                        new RandomBlockMatchTest(Blocks.IRON_BARS, 0.2F),
+                        AlwaysTrueTest.INSTANCE,
+                        Blocks.IRON_BARS.defaultBlockState().setValue(CrossCollisionBlock.WATERLOGGED, true)
+                )
+        ));
     }
+
+
 }
