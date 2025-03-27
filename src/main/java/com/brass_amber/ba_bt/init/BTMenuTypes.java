eb849749d9a97cc29f0f185de1dbca248669b1a8
JMilamber <socky79.turbulance@gmail.com>
2025-03-27 14:53:57 -0400

    Rename Containertypes to menuTypes

---
+++ src/main/java/com/brass_amber/ba_bt/init/BTMenuTypes.java
@@ -0,0 +1,31 @@
+package com.brass_amber.ba_bt.init;
+
+import com.brass_amber.ba_bt.BABTMain;
+import com.brass_amber.ba_bt.inventory.BTChestMenu;
+import com.brass_amber.ba_bt.inventory.DataMarkerMenu;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.network.IContainerFactory;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class BTMenuTypes {
+
+    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, BABTMain.MODID);
+
+    public static final RegistryObject<MenuType<BTChestMenu>> GENERIC_9x8 = MENUS.register("generic_9x8", () -> new MenuType<>(BTChestMenu::eightRows, FeatureFlags.REGISTRY.allFlags()));
+
+    public static final RegistryObject<MenuType<DataMarkerMenu>> DATA_MARKER_MENU = registerMenuType(DataMarkerMenu::new, "data_marker_menu");
+
+    private static  <T extends AbstractContainerMenu>RegistryObject<MenuType<T>> registerMenuType(IContainerFactory<T> factory, String name) {
+        return MENUS.register(name, () -> IForgeMenuType.create(factory));
+    }
+
+    public static void register(IEventBus eventBus) {
+        MENUS.register(eventBus);
+    }
+}
--- src/main/java/com/brass_amber/ba_bt/inventory/BTChestMenu.java
+++ src/main/java/com/brass_amber/ba_bt/inventory/BTChestMenu.java
@@ -1,6 +1,6 @@
 package com.brass_amber.ba_bt.inventory;
 
-import com.brass_amber.ba_bt.init.BTContainerTypes;
+import com.brass_amber.ba_bt.init.BTMenuTypes;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
@@ -14,11 +14,11 @@ public class BTChestMenu extends ChestMenu {
     }
 
     public static BTChestMenu eightRows(int containerId, Inventory playerInventory) {
-        return new BTChestMenu(BTContainerTypes.GENERIC_9x8.get(),  containerId, playerInventory, 8);
+        return new BTChestMenu(BTMenuTypes.GENERIC_9x8.get(),  containerId, playerInventory, 8);
     }
 
     public static BTChestMenu eightRows(int containerId, Inventory playerInventory, Container container) {
-        return new BTChestMenu(BTContainerTypes.GENERIC_9x8.get(),  containerId, playerInventory, container, 8);
+        return new BTChestMenu(BTMenuTypes.GENERIC_9x8.get(),  containerId, playerInventory, container, 8);
     }
 
     private BTChestMenu(MenuType<?> menuType, int containerId, Inventory playerInventory, Container container, int rows) {
