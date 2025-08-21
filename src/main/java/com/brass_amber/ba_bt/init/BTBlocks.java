7917a928192d9c3207798bc39cdc5a7e2a23c263
JMilamber <socky79.turbulance@gmail.com>
2025-08-21 14:02:23 -0400

    make Corrite blocks use properties.copyOf for easier changing

--- src/main/java/com/brass_amber/ba_bt/init/BTBlocks.java
+++ src/main/java/com/brass_amber/ba_bt/init/BTBlocks.java
@@ -122,21 +122,21 @@ public class BTBlocks {
 
 
 	public static final RegistryObject<Block> CORRITE_BLOCK = registerBlock("corrite_block",
-			() -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(0.5F)), 64);
+			() -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(15F, 1200.0F)), 64);
 
 	public static final RegistryObject<Block> CORRITE_STAIR = registerBlock("corrite_stair",
-			() -> new StairBlock(CORRITE_BLOCK.get()::defaultBlockState, BlockBehaviour.Properties.of().mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(0.5F)), 64);
+			() -> new StairBlock(CORRITE_BLOCK.get()::defaultBlockState, BlockBehaviour.Properties.copy(CORRITE_BLOCK.get())), 64);
 
 	public static final RegistryObject<Block> CORRITE_SLAB = registerBlock("corrite_slab",
-			() -> new SlabBlock(BlockBehaviour.Properties.of().mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(0.5F)), 64);
+			() -> new SlabBlock(BlockBehaviour.Properties.copy(CORRITE_BLOCK.get())), 64);
 
 	public static final RegistryObject<Block> CORRITE_WALL = registerBlock("corrite_wall",
-			() -> new WallBlock(BlockBehaviour.Properties.of().mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(0.5F)), 64);
+			() -> new WallBlock(BlockBehaviour.Properties.copy(CORRITE_BLOCK.get())), 64);
 
 
 	public static final RegistryObject<Block> ACTIVE_CORRITE_BLOCK = registerBlock("active_corrite_block",
 			() -> new ActiveCorriteBlock(
-					Block.Properties.of().mapColor(MapColor.NETHER).strength(2.5F, 1200.0F).sound(SoundType.DEEPSLATE)
+					Block.Properties.of().mapColor(MapColor.NETHER).strength(5F, 1200.0F).sound(SoundType.DEEPSLATE)
 							.requiresCorrectToolForDrops().lightLevel((blockState) -> 3)
 							.isValidSpawn((blockState, blockGetter, blockPos, entityType) -> entityType.fireImmune())
 							.hasPostProcess(BTBlocks::always).emissiveRendering(BTBlocks::always)), 64);
