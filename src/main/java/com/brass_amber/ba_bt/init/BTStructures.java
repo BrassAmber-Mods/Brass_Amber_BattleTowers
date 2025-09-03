b36f759e2089a2f235b4672a4288c6d3b8f5247f
JMilamber <socky79.turbulance@gmail.com>
2025-09-03 14:28:39 -0400

    Add tower resource keys

--- src/main/java/com/brass_amber/ba_bt/init/BTStructures.java
+++ src/main/java/com/brass_amber/ba_bt/init/BTStructures.java
@@ -7,6 +7,7 @@ import com.brass_amber.ba_bt.worldGen.structures.LandTower;
 import com.brass_amber.ba_bt.worldGen.structures.OceanTower;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,11 +21,18 @@ public class BTStructures {
 
     /**
      * Registers the base structure itself and sets what its path is. In this case,
-     * this base structure will have the resourcelocation of babt:land_tower.
+     * this base structure will have the resourcelocation of ba_bt:land_tower.
      */
     public static final RegistryObject<StructureType<LandTower>> LAND_TOWER = STRUCTURE_REGISTRY.register("land_tower", () -> explicitStructureTypeTyping(LandTower.CODEC));
     public static final RegistryObject<StructureType<OceanTower>> OCEAN_TOWER = STRUCTURE_REGISTRY.register("ocean_tower", () -> explicitStructureTypeTyping(OceanTower.CODEC));
     public static final RegistryObject<StructureType<CoreTower>> CORE_TOWER = STRUCTURE_REGISTRY.register("core_tower", () -> explicitStructureTypeTyping(CoreTower.CODEC));
+
+    /**
+     * Resource key for referencing these structures like #net.minecraft.world.level.levelgen.structure.BuiltinStructures
+     */
+    public static final ResourceKey<Structure> LAND_TOWER_KEY = createKey("land_tower");
+    public static final ResourceKey<Structure> OCEAN_TOWER_KEY = createKey("ocean_tower");
+    public static final ResourceKey<Structure> CORE_TOWER_KEY = createKey("core_tower");
 
     /**
      * This method explicitly states what the return type
@@ -36,6 +44,10 @@ public class BTStructures {
 
     public static void register(IEventBus eventBus) {
         STRUCTURE_REGISTRY.register(eventBus);
+    }
+
+    private static ResourceKey<Structure> createKey(String name) {
+        return ResourceKey.create(Registries.STRUCTURE, BABattleTowers.locate(name));
     }
 
 }
