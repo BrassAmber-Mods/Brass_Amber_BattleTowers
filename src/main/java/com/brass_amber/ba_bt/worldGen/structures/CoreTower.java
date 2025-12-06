c264d3e5441645e3e9923eee1af1b114f129e395
JMilamber <socky79.turbulance@gmail.com>
2025-12-05 22:09:23 -0500

    move biome checking out of checkVariant

--- src/main/java/com/brass_amber/ba_bt/worldGen/structures/CoreTower.java
+++ src/main/java/com/brass_amber/ba_bt/worldGen/structures/CoreTower.java
@@ -88,36 +88,56 @@ public class CoreTower extends Structure implements TowerStructure{
         ChunkPos chunkPos = generationContext.chunkPos();
         ChunkGenerator chunkGen = generationContext.chunkGenerator();
 
+
         int middleHieght = chunkGen.getFirstOccupiedHeight(
                 chunkPos.getMiddleBlockX(), chunkPos.getMiddleBlockZ(), Heightmap.Types.WORLD_SURFACE_WG, generationContext.heightAccessor(), generationContext.randomState()
         );
+
+        BlockPos blockpos = chunkPos.getMiddleBlockPosition(middleHieght);
 
         Holder<Biome> biome = generationContext.biomeSource().getNoiseBiome(
                 QuartPos.fromBlock(chunkPos.getMiddleBlockX()), QuartPos.fromBlock(middleHieght), QuartPos.fromBlock(chunkPos.getMiddleBlockZ()), generationContext.randomState().sampler()
         );
 
-        if (isValidBiome(generationContext, chunkPos.getMiddleBlockPosition(middleHieght), biome)) {
+        HolderSet<Biome> holderset = generationContext.registryAccess().registryOrThrow(Registries.BIOME).getTag(BiomeTags.IS_OCEAN).orElseThrow();
+        Predicate<Holder<Biome>> predicate = holderset::contains;
+        Pair<BlockPos, Holder<Biome>> oceanBiomeNearby = generationContext.chunkGenerator().getBiomeSource().findBiomeHorizontal(
+                blockpos.getX(), generationContext.chunkGenerator().getSeaLevel(), blockpos.getZ(), 77,
+                predicate, generationContext.random(), generationContext.randomState().sampler()
+        );
+
+        holderset = generationContext.registryAccess().registryOrThrow(Registries.BIOME).getTag(BTTags.Biomes.CORE_TOWER_MOUNTAIN_BIOMES).orElseThrow();
+        predicate = holderset::contains;
+        Pair<BlockPos, Holder<Biome>> mountainBiomeNearby = generationContext.chunkGenerator().getBiomeSource().findBiomeHorizontal(
+                blockpos.getX(), generationContext.chunkGenerator().getSeaLevel() + 20, blockpos.getZ(), 400,
+                predicate, generationContext.random(), generationContext.randomState().sampler()
+        );
+
+        holderset = generationContext.registryAccess().registryOrThrow(Registries.BIOME).getTag(BiomeTags.HAS_ANCIENT_CITY).orElseThrow();
+        predicate = holderset::contains;
+        Pair<BlockPos, Holder<Biome>> sculkBiomeNearby = generationContext.chunkGenerator().getBiomeSource().findBiomeHorizontal(
+                blockpos.getX(), -60, blockpos.getZ(), 64,
+                predicate, generationContext.random(), generationContext.randomState().sampler()
+        );
+
+        BABattleTowers.LOGGER.debug(
+                "Is Valid Core Tower Biome? {} distance {}, {} distance {}, {} distance {}",
+                oceanBiomeNearby != null ? oceanBiomeNearby.getSecond().unwrapKey().get().location().getPath() : "none", oceanBiomeNearby != null ? BTUtil.distanceTo2DInt(blockpos, oceanBiomeNearby.getFirst()) : 0,
+                mountainBiomeNearby != null ? mountainBiomeNearby.getSecond().unwrapKey().get().location().getPath() : "none", mountainBiomeNearby != null ? BTUtil.distanceTo2DInt(blockpos, mountainBiomeNearby.getFirst()) : 0,
+                sculkBiomeNearby != null ? sculkBiomeNearby.getSecond().unwrapKey().get().location().getPath() : "none", sculkBiomeNearby != null ? BTUtil.distanceTo2DInt(blockpos, sculkBiomeNearby.getFirst()) : 0);
+
+
+        if (generationContext.validBiome().test(biome) && oceanBiomeNearby == null && mountainBiomeNearby != null && sculkBiomeNearby == null) {
             // BrassAmberBattleTowers.LOGGER.debug("Bad Biome for Ocean: " + biome.unwrapKey() + " " + pos);
+            checkVariant(generationContext, blockpos);
             return Pair.of(true, middleHieght);
         }
 
         return Pair.of(false, 0);
     }
 
-    public boolean isValidBiome(GenerationContext context, BlockPos blockpos, Holder<Biome> biomeHolder) {
+    public void checkVariant(GenerationContext context, BlockPos blockpos) {
         // BABattleTowers.LOGGER.debug("Is Valid Core Tower Biome");
-        HolderSet<Biome> holderset = context.registryAccess().registryOrThrow(Registries.BIOME).getTag(BiomeTags.IS_OCEAN).orElseThrow();
-        Predicate<Holder<Biome>> predicate = holderset::contains;
-        Pair<BlockPos, Holder<Biome>> oceanBiomeNearby = context.chunkGenerator().getBiomeSource().findBiomeHorizontal(blockpos.getX(), context.chunkGenerator().getSeaLevel(), blockpos.getZ(), 77, predicate, context.random(), context.randomState().sampler());
-
-        holderset = context.registryAccess().registryOrThrow(Registries.BIOME).getTag(BTTags.Biomes.CORE_TOWER_MOUNTAIN_BIOMES).orElseThrow();
-        predicate = holderset::contains;
-        Pair<BlockPos, Holder<Biome>> mountainBiomeNearby = context.chunkGenerator().getBiomeSource().findBiomeHorizontal(blockpos.getX(), context.chunkGenerator().getSeaLevel() + 20, blockpos.getZ(), 400, predicate, context.random(), context.randomState().sampler());
-
-        holderset = context.registryAccess().registryOrThrow(Registries.BIOME).getTag(BiomeTags.HAS_ANCIENT_CITY).orElseThrow();
-        predicate = holderset::contains;
-        Pair<BlockPos, Holder<Biome>> sculkBiomeNearby = context.chunkGenerator().getBiomeSource().findBiomeHorizontal(blockpos.getX(), -60, blockpos.getZ(), 64, predicate, context.random(), context.randomState().sampler());
-
 
         WorldgenRandom worldgenRandom = context.random();
         worldgenRandom.setSeed(context.seed());
@@ -127,13 +147,6 @@ public class CoreTower extends Structure implements TowerStructure{
             // Gilded or Island
             this.towerType = randomSource.nextFloat() > .8 ? 1 : 0;
         }
-        BABattleTowers.LOGGER.debug(
-                "Is Valid Core Tower Biome? {} distance {}, {} distance {}, {} distance {}",
-                oceanBiomeNearby != null ? oceanBiomeNearby.getSecond().unwrapKey().get().location().getPath() : "none", oceanBiomeNearby != null ? BTUtil.distanceTo2DInt(blockpos, oceanBiomeNearby.getFirst()) : 0,
-                mountainBiomeNearby != null ? mountainBiomeNearby.getSecond().unwrapKey().get().location().getPath() : "none", mountainBiomeNearby != null ? BTUtil.distanceTo2DInt(blockpos, mountainBiomeNearby.getFirst()) : 0,
-                sculkBiomeNearby != null ? sculkBiomeNearby.getSecond().unwrapKey().get().location().getPath() : "none", sculkBiomeNearby != null ? BTUtil.distanceTo2DInt(blockpos, sculkBiomeNearby.getFirst()) : 0);
-
-        return context.validBiome().test(biomeHolder) && oceanBiomeNearby == null && mountainBiomeNearby != null && sculkBiomeNearby == null;
     }
 
     @Override
