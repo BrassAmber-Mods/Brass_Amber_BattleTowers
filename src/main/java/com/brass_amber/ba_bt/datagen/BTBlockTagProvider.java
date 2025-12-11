251a0fe0d4973ed4d28663abd685c6c22cf28e85
JMilamber <socky79.turbulance@gmail.com>
2025-12-11 12:30:01 -0500

    add corrite wall to wall block tag

--- src/main/java/com/brass_amber/ba_bt/datagen/BTBlockTagProvider.java
+++ src/main/java/com/brass_amber/ba_bt/datagen/BTBlockTagProvider.java
@@ -4,11 +4,8 @@ import com.brass_amber.ba_bt.BABattleTowers;
 import com.brass_amber.ba_bt.init.BTBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.nbt.Tag;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -16,6 +13,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.concurrent.CompletableFuture;
 
 import static com.brass_amber.ba_bt.util.BTTags.Blocks.*;
+import static net.minecraft.tags.BlockTags.WALLS;
 import static net.minecraftforge.common.Tags.Blocks.CHESTS;
 
 public class BTBlockTagProvider extends BlockTagsProvider  {
@@ -42,6 +40,8 @@ public class BTBlockTagProvider extends BlockTagsProvider  {
         );
 
         this.tag(CHESTS).addTag(BT_CHESTS);
+
+        this.tag(WALLS).add(BTBlocks.CORRITE_WALL.get());
 
         this.tag(BT_SPAWNERS).add(
                 BTBlocks.LAND_SPAWNER.get(),
