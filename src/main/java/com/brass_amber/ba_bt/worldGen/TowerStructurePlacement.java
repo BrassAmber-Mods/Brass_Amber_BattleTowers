3cfdd73868e6b6d08e98ca531dbd352622a875ca
JMilamber <socky79.turbulance@gmail.com>
2025-12-29 19:05:32 -0500

    Adjust structure placement to allow for towers within the first tower distance if the seperation is larger than it

--- src/main/java/com/brass_amber/ba_bt/worldGen/TowerStructurePlacement.java
+++ src/main/java/com/brass_amber/ba_bt/worldGen/TowerStructurePlacement.java
@@ -11,6 +11,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.ChunkGeneratorStructureState;
+import net.minecraft.world.level.levelgen.LegacyRandomSource;
+import net.minecraft.world.level.levelgen.WorldgenRandom;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
 import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
@@ -20,6 +22,7 @@ import net.minecraft.world.level.levelgen.structure.placement.StructurePlacement
 import java.util.Optional;
 
 import static com.brass_amber.ba_bt.init.BTStructurePlacements.TOWER_STRUCTURE_PLACEMENT;
+import static com.brass_amber.ba_bt.util.BTUtil.chunkDistanceTo;
 
 public class TowerStructurePlacement extends RandomSpreadStructurePlacement {
 
@@ -77,10 +80,10 @@ public class TowerStructurePlacement extends RandomSpreadStructurePlacement {
      * Reference taken from TelepathicGrunt DistanceBasedStructurePlacement class, uses config value instead of placement variable
      */
     @Override
-    protected boolean isPlacementChunk(ChunkGeneratorStructureState chunkGeneratorStructureState, int x, int z) {
+    protected boolean isPlacementChunk(ChunkGeneratorStructureState chunkGeneratorStructureState, int chunkX, int chunkZ) {
 
         // Simple fast distance check without needing to do a square root. The threshold is circular around world origin.
-        if (((long) x * x) + ((long) z * z) < (((long) BattleTowersConfig.firstTowerDistance) * BattleTowersConfig.firstTowerDistance)) {
+        if (((long) chunkX * chunkX) + ((long) chunkZ * chunkZ) < (((long) BattleTowersConfig.firstTowerDistance) * BattleTowersConfig.firstTowerDistance)) {
             // BABattleTowers.LOGGER.debug("Structure {} within first tower distance", chunkGeneratorStructureState.possibleStructureSets().get(0).unwrapKey().get().location().getPath());
             return false;
         }
@@ -88,15 +91,30 @@ public class TowerStructurePlacement extends RandomSpreadStructurePlacement {
         boolean hasStructure = false;
 
         for (Holder<StructureSet> set : avoidStructures) {
-            hasStructure = chunkGeneratorStructureState.hasStructureChunkInRange(set, x, z, minDistanceFromAvoidStructures);
+            hasStructure = chunkGeneratorStructureState.hasStructureChunkInRange(set, chunkX, chunkZ, minDistanceFromAvoidStructures);
             if (hasStructure) {
                 // BABattleTowers.LOGGER.debug("Structure from set {} in range", set.unwrapKey().get().location().getPath());
                 break;
             }
         }
 
-        ChunkPos chunkpos = this.getPotentialStructureChunk(chunkGeneratorStructureState.getLevelSeed(), x, z);
-        return chunkpos.x == x && chunkpos.z == z && !hasStructure;
+        ChunkPos chunkpos = this.getPotentialStructureChunkUseLowest(chunkGeneratorStructureState.getLevelSeed(), chunkX, chunkZ);
+        return chunkpos.x == chunkX && chunkpos.z == chunkZ && !hasStructure;
+    }
+
+    public ChunkPos getPotentialStructureChunkUseLowest(long seed, int chunkX, int chunkZ) {
+        int i = Math.floorDiv(chunkX, this.spacing());
+        int j = Math.floorDiv(chunkZ, this.spacing());
+        WorldgenRandom worldgenrandom = new WorldgenRandom(new LegacyRandomSource(0L));
+        worldgenrandom.setLargeFeatureWithSalt(seed, i, j, this.salt());
+        int k = this.spacing() - this.separation();
+        // Allow placements between first tower distance and minimum seperation
+        if (this.separation() > chunkDistanceTo(ChunkPos.ZERO , new ChunkPos(chunkX, chunkZ)) && this.separation() > BattleTowersConfig.firstTowerDistance) {
+            k = this.spacing() - BattleTowersConfig.firstTowerDistance;
+        }
+        int l = this.spreadType().evaluate(worldgenrandom, k);
+        int i1 = this.spreadType().evaluate(worldgenrandom, k);
+        return new ChunkPos(i * this.spacing() + l, j * this.spacing() + i1);
     }
 
     public StructurePlacementType<?> type() {
