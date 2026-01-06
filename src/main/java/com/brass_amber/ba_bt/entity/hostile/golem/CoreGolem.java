45f22efdd7af17c10bab04a862ff699378001e62
JMilamber <socky79.turbulance@gmail.com>
2026-01-06 14:45:13 -0500

    Core golem heals by 1/4 max health when entering unleashed stage

--- src/main/java/com/brass_amber/ba_bt/entity/hostile/golem/CoreGolem.java
+++ src/main/java/com/brass_amber/ba_bt/entity/hostile/golem/CoreGolem.java
@@ -133,6 +133,7 @@ public class CoreGolem extends AbstractGolem {
 			BABattleTowers.LOGGER.debug("fix ai");
 			this.resetAi = false;
 			this.goalSelector.setNewGoalRate(3);
+			this.heal(this.getMaxHealth() / 4);
 		}
 
 		if (this.level().isClientSide()) {
