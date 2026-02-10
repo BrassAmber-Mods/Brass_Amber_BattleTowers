6c83be315526aaf1fbe7bec1bcda6c662bd15dd4
JMilamber <socky79.turbulance@gmail.com>
2026-02-09 22:06:05 -0500

    Add Spawner data to tower type, as well as music references and crumble percent.

--- src/main/java/com/brass_amber/ba_bt/util/TowerType.java
+++ src/main/java/com/brass_amber/ba_bt/util/TowerType.java
@@ -6,52 +6,181 @@ import com.brass_amber.ba_bt.block.blockentity.*;
 import com.brass_amber.ba_bt.entity.CoreDestructionEntity;
 import com.brass_amber.ba_bt.entity.LandDestructionEntity;
 import com.brass_amber.ba_bt.entity.OceanDestructionEntity;
+import com.brass_amber.ba_bt.entity.block.AbstractObeliskEntity;
 import com.brass_amber.ba_bt.entity.block.BTAbstractObelisk;
 import com.brass_amber.ba_bt.entity.block.BTMonolith;
 import com.brass_amber.ba_bt.entity.AbstractDestructionEntity;
-import com.brass_amber.ba_bt.entity.hostile.BTCultist;
 import com.brass_amber.ba_bt.init.BTBlockEntityType;
 import com.brass_amber.ba_bt.init.BTBlocks;
 import com.brass_amber.ba_bt.init.BTEntityType;
 import com.brass_amber.ba_bt.init.BTItems;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.Music;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Locale;
 
 import static com.brass_amber.ba_bt.BattleTowersConfig.*;
+import static com.brass_amber.ba_bt.sound.BTMusic.*;
 
 public enum TowerType implements StringRepresentable {
-	LAND("LAND", Component.translatable("entity.ba_bt.land_golem"), "#9BDAE7"),
-	OCEAN("OCEAN", Component.translatable("entity.ba_bt.ocean_golem"), "#EAE78A"),
-	CORE("CORE", Component.translatable("entity.ba_bt.core_golem"), "#F79B3A"),
-	NETHER("NETHER", Component.translatable("entity.ba_bt.nether_golem"), "#88EB63"),
-	END("END", Component.translatable("entity.ba_bt.end_golem"), "#BA49EF"),
-	SKY("SKY", Component.translatable("entity.ba_bt.sky_golem"), "#FBC1EB"),
-	CITY("CITY", Component.literal("~"), ""),
-	EMPTY("EMPTY",Component.literal("Empty"), "");
+	LAND("LAND", Component.translatable("entity.ba_bt.land_golem"), "#9BDAE7", landTowerMobs,
+			LAND_TOWER_MUSIC, LAND_GOLEM_FIGHT_MUSIC, 98, landTowerCrumblePercent,
+			new TowerFloor[]{
+					new TowerFloor(2, 200, 220, 1, 4, 11, 6),
+					new TowerFloor(2, 195, 215, 1, 4, 11, 6),
+					new TowerFloor(2, 190, 210, 2, 4, 12, 6),
+					new TowerFloor(2, 185, 205, 2, 4, 12, 6),
+					new TowerFloor(3, 180, 200, 3, 4, 12, 6),
+					new TowerFloor(3, 175, 195, 3, 4, 12, 6),
+					new TowerFloor(3, 170, 190, 3, 4, 13, 6),
+					new TowerFloor(4, 165, 185, 3, 4, 13, 6),
+			}
+	),
+	OCEAN("OCEAN", Component.translatable("entity.ba_bt.ocean_golem"), "#EAE78A", oceanTowerMobs,
+			OCEAN_TOWER_MUSIC, OCEAN_GOLEM_FIGHT_MUSIC, -110, oceanTowerCrumblePercent,
+			new TowerFloor[]{
+					new TowerFloor(2, 240, 280, 2, 4, 12, 6),
+					new TowerFloor(2, 235, 275, 2, 4, 12, 6),
+					new TowerFloor(2, 230, 270, 3, 4, 13, 6),
+					new TowerFloor(3, 225, 265, 3, 4, 13, 6),
+					new TowerFloor(3, 220, 260, 3, 4, 13, 6),
+					new TowerFloor(3, 215, 255, 3, 4, 13, 6),
+					new TowerFloor(4, 210, 250, 4, 4, 14, 6),
+					new TowerFloor(4, 205, 245, 4, 4, 14, 6),
+			}
+	),
+	CORE("CORE", Component.translatable("entity.ba_bt.core_golem"), "#F79B3A", coreTowerMobs,
+			CORE_TOWER_MUSIC, CORE_GOLEM_FIGHT_MUSIC, 98, coreTowerCrumblePercent,
+			new TowerFloor[]{
+					new TowerFloor(2, 200, 220, 2, 5, 11, 6),
+					new TowerFloor(2, 195, 215, 2, 5, 11, 6),
+					new TowerFloor(3, 190, 210, 3, 5, 12, 6),
+					new TowerFloor(3, 185, 205, 3, 5, 12, 6),
+					new TowerFloor(3, 180, 200, 3, 6, 12, 6),
+					new TowerFloor(3, 175, 195, 3, 6, 12, 6),
+					new TowerFloor(4, 170, 190, 4, 6, 13, 6),
+					new TowerFloor(4, 165, 185, 4, 6, 13, 6),
+			}
+	),
+	NETHER("NETHER", Component.translatable("entity.ba_bt.nether_golem"), "#88EB63", netherTowerMobs,
+			NETHER_TOWER_MUSIC, NETHER_GOLEM_FIGHT_MUSIC, 98, netherTowerCrumblePercent,
+			new TowerFloor[]{
+					new TowerFloor(2, 200, 220, 1, 4, 11, 6),
+					new TowerFloor(2, 195, 215, 1, 4, 11, 6),
+					new TowerFloor(2, 190, 210, 2, 4, 12, 6),
+					new TowerFloor(2, 185, 205, 2, 4, 12, 6),
+					new TowerFloor(3, 180, 200, 3, 4, 12, 6),
+					new TowerFloor(3, 175, 195, 3, 4, 12, 6),
+					new TowerFloor(3, 170, 190, 3, 4, 13, 6),
+					new TowerFloor(4, 165, 185, 3, 4, 13, 6),
+			}
+	),
+	END("END", Component.translatable("entity.ba_bt.end_golem"), "#BA49EF", endTowerMobs,
+			END_TOWER_MUSIC, END_GOLEM_FIGHT_MUSIC, 98, endTowerCrumblePercent,
+			new TowerFloor[]{
+					new TowerFloor(2, 200, 220, 1, 4, 11, 6),
+					new TowerFloor(2, 195, 215, 1, 4, 11, 6),
+					new TowerFloor(2, 190, 210, 2, 4, 12, 6),
+					new TowerFloor(2, 185, 205, 2, 4, 12, 6),
+					new TowerFloor(3, 180, 200, 3, 4, 12, 6),
+					new TowerFloor(3, 175, 195, 3, 4, 12, 6),
+					new TowerFloor(3, 170, 190, 3, 4, 13, 6),
+					new TowerFloor(4, 165, 185, 3, 4, 13, 6),
+			}
+	),
+	SKY("SKY", Component.translatable("entity.ba_bt.sky_golem"), "#FBC1EB", skyTowerMobs,
+			SKY_TOWER_MUSIC, SKY_GOLEM_FIGHT_MUSIC, 98, skyTowerCrumblePercent,
+			new TowerFloor[]{
+					new TowerFloor(2, 200, 220, 1, 4, 11, 6),
+					new TowerFloor(2, 195, 215, 1, 4, 11, 6),
+					new TowerFloor(2, 190, 210, 2, 4, 12, 6),
+					new TowerFloor(2, 185, 205, 2, 4, 12, 6),
+					new TowerFloor(3, 180, 200, 3, 4, 12, 6),
+					new TowerFloor(3, 175, 195, 3, 4, 12, 6),
+					new TowerFloor(3, 170, 190, 3, 4, 13, 6),
+					new TowerFloor(4, 165, 185, 3, 4, 13, 6),
+			}
+	),
+	CITY("CITY", Component.literal("~"), "", List.of(),
+			LAND_TOWER_MUSIC, LAND_GOLEM_FIGHT_MUSIC, 98, landTowerCrumblePercent,
+			new TowerFloor[]{}
+	),
+	AETHER("AETHER", Component.literal("~"), "", List.of(),
+			LAND_TOWER_MUSIC, LAND_GOLEM_FIGHT_MUSIC, 98, landTowerCrumblePercent,
+			new TowerFloor[]{}
+	),
+	DEEPER_DARK("DEEPER_DARK", Component.literal("~"), "", List.of(),
+			LAND_TOWER_MUSIC, LAND_GOLEM_FIGHT_MUSIC, 98, landTowerCrumblePercent,
+			new TowerFloor[]{}
+	),
+	EMPTY("EMPTY",Component.literal("Empty"), "", List.of(),
+			LAND_TOWER_MUSIC, LAND_GOLEM_FIGHT_MUSIC, 98, landTowerCrumblePercent,
+			new TowerFloor[]{}
+	);
 
 	private final String name;
 	private final Component displayName;
 	private final String colorCode;
+	private final List<EntityType<?>> towerMobs;
+	private final TowerFloor[] towerFloors;
+	private final Music towerMusic;
+	private final Music bossMusic;
+	private final int destructionOffset;
+	private final double destructionPercent;
 
-	TowerType(String name, Component displayName, String colorCode) {
+	TowerType(String name, Component displayName, String colorCode, List<EntityType<?>> towerMobs, Music towerMusic, Music bossMusic, int destructionOffset, double destructionPercent, TowerFloor[] towerFloors) {
 		this.name = name;
 		this.displayName = displayName;
 		this.colorCode = colorCode;
+		this.towerMobs = towerMobs;
+		this.towerFloors = towerFloors;
+		this.towerMusic = towerMusic;
+		this.bossMusic = bossMusic;
+		this.destructionOffset = destructionOffset;
+		this.destructionPercent = destructionPercent;
 	}
 
 	public static final TowerType[] VALUES = values();
+
+	public List<EntityType<?>> getTowerMobs() {
+		return towerMobs;
+	}
+
+	public TowerFloor[] getTowerFloors() {
+		return towerFloors;
+	}
+
+	public TowerFloor getTowerFloor(int floorNum) {
+		return towerFloors[floorNum];
+	}
+
+	public Music getTowerMusic() {
+		return towerMusic;
+	}
+
+	public Music getBossMusic() {
+		return bossMusic;
+	}
+
+
+	public int getDestructionOffset() {
+		return destructionOffset;
+	}
+
+	public double getDestructionPercent() {
+		return destructionPercent;
+	}
+
 
 	/*********************************************************** Monolith Spawning ********************************************************/
 
@@ -60,12 +189,12 @@ public enum TowerType implements StringRepresentable {
 	 */
 	public static @NotNull EntityType<?> getGolemFor(TowerType towerType) {
 		return switch (towerType) {
-			case EMPTY, CITY, LAND -> BTEntityType.LAND_GOLEM.get();
 			case OCEAN -> BTEntityType.OCEAN_GOLEM.get();
 			case CORE -> BTEntityType.CORE_GOLEM.get();
 			case NETHER -> BTEntityType.NETHER_GOLEM.get();
 			case END -> BTEntityType.END_GOLEM.get();
 			case SKY -> BTEntityType.SKY_GOLEM.get();
+			default -> BTEntityType.LAND_GOLEM.get();
 		};
 	}
 
@@ -74,12 +203,12 @@ public enum TowerType implements StringRepresentable {
 	 */
 	public static @NotNull EntityType<BTMonolith> getMonolithFor(TowerType towerType) {
 		return switch (towerType) {
-			case EMPTY, CITY, LAND -> BTEntityType.LAND_MONOLITH.get();
 			case OCEAN -> BTEntityType.OCEAN_MONOLITH.get();
 			case CORE -> BTEntityType.CORE_MONOLITH.get();
 			case NETHER -> BTEntityType.NETHER_MONOLITH.get();
 			case END -> BTEntityType.END_MONOLITH.get();
 			case SKY -> BTEntityType.SKY_MONOLITH.get();
+			default -> BTEntityType.LAND_MONOLITH.get();
 		};
 	}
 
@@ -91,34 +220,26 @@ public enum TowerType implements StringRepresentable {
 	@NotNull
 	public static EntityType<BTAbstractObelisk> getObeliskFor(TowerType towerType) {
 		return switch (towerType) {
-			case EMPTY, CITY, LAND -> BTEntityType.LAND_OBELISK.get();
 			case OCEAN -> BTEntityType.OCEAN_OBELISK.get();
 			case CORE -> BTEntityType.CORE_OBELISK.get();
 			case NETHER -> BTEntityType.NETHER_OBELISK.get();
 			case END -> BTEntityType.END_OBELISK.get();
 			case SKY -> BTEntityType.SKY_OBELISK.get();
+			default -> BTEntityType.LAND_OBELISK.get();
 		};
 	}
 
 	@NotNull
-	public static TowerType getTypeForObelisk(BTAbstractObelisk entity) {
-		EntityType<?> entityType = entity.getType();
-        if (entityType.equals(BTEntityType.LAND_OBELISK.get())) {
-            return LAND;
-        } else if (entityType.equals(BTEntityType.OCEAN_OBELISK.get())) {
-            return OCEAN;
-        } else if (entityType.equals(BTEntityType.CORE_OBELISK.get())) {
-            return CORE;
-        } else if (entityType.equals(BTEntityType.NETHER_OBELISK.get())) {
-            return NETHER;
-        } else if (entityType.equals(BTEntityType.END_OBELISK.get())) {
-            return END;
-        } else if (entityType.equals(BTEntityType.SKY_OBELISK.get())) {
-            return SKY;
-        }
-		return EMPTY;
-    }
-
+	public  static EntityType<AbstractDestructionEntity> getDestructionEntityFor(TowerType towerType) {
+		return switch (towerType) {
+			case OCEAN -> BTEntityType.OCEAN_DESTRUCTION.get();
+			case CORE -> BTEntityType.CORE_DESTRUCTION.get();
+			case NETHER -> BTEntityType.NETHER_DESTRUCTION.get();
+			case END -> BTEntityType.END_DESTRUCTION.get();
+			case SKY -> BTEntityType.SKY_DESTRUCTION.get();
+			default -> BTEntityType.LAND_DESTRUCTION.get();
+		};
+	}
 
 	/*********************************************************** Monolith ********************************************************/
 
@@ -128,13 +249,13 @@ public enum TowerType implements StringRepresentable {
 
 	public static Item getMonolithItemFor(TowerType towerType) {
 		return switch (towerType) {
-			default -> BTItems.LAND_MONOLITH.get();
-			case OCEAN -> BTItems.OCEAN_MONOLITH.get();
+            case OCEAN -> BTItems.OCEAN_MONOLITH.get();
 			case CORE -> BTItems.CORE_MONOLITH.get();
 			case NETHER -> BTItems.NETHER_MONOLITH.get();
 			case END -> BTItems.END_MONOLITH.get();
 			case SKY -> BTItems.SKY_MONOLITH.get();
-		};
+            default -> BTItems.LAND_MONOLITH.get();
+        };
 	}
 
 	/**
@@ -225,6 +346,27 @@ public enum TowerType implements StringRepresentable {
 		// Couldn't get EntityType
 		return EMPTY;
     }
+
+	public static TowerType getTypeForObeliskEntity(AbstractObeliskEntity entity) {
+		EntityType<?> entityType = entity.getType();
+
+		if (entityType.equals(BTEntityType.LAND_OBELISK.get())) {
+			return LAND;
+		} else if (entityType.equals(BTEntityType.OCEAN_OBELISK.get())) {
+			return OCEAN;
+		} else if (entityType.equals(BTEntityType.CORE_OBELISK.get())) {
+			return CORE;
+		} else if (entityType.equals(BTEntityType.NETHER_OBELISK.get())) {
+			return NETHER;
+		} else if (entityType.equals(BTEntityType.END_OBELISK.get())) {
+			return END;
+		} else if (entityType.equals(BTEntityType.SKY_OBELISK.get())) {
+			return SKY;
+		}
+
+		// Couldn't get EntityType
+		return EMPTY;
+	}
 	/**
 	 * Return the correct ChestBlock Entity for each GolemType.
 	 */
@@ -311,13 +453,13 @@ public enum TowerType implements StringRepresentable {
 	@Nullable
 	public static Item getEyeFor(TowerType towerType) {
 		return switch (towerType) {
-			case EMPTY, CITY -> null;
 			case LAND -> BTItems.LAND_GOLEM_EYE.get();
 			case OCEAN -> BTItems.OCEAN_GOLEM_EYE.get();
 			case CORE -> BTItems.CORE_GOLEM_EYE.get();
 			case NETHER -> BTItems.NETHER_GOLEM_EYE.get();
 			case END -> BTItems.END_GOLEM_EYE.get();
 			case SKY -> BTItems.SKY_GOLEM_EYE.get();
+			default -> null;
 		};
 	}
 
@@ -325,15 +467,9 @@ public enum TowerType implements StringRepresentable {
 	 * Return the previous GolemType in fighting order.
 	 */
 
-	public static TowerType getPreviousGolemType(TowerType towerType) {
-		return switch (towerType) {
-			case OCEAN -> LAND;
-			case CORE -> OCEAN;
-			case NETHER -> CORE;
-			case END -> NETHER;
-			case SKY -> END;
-			default -> EMPTY;
-		};
+	public TowerType getPreviousGolemType() {
+		int ordinalVal = this.ordinal() - 1;
+		return ordinalVal > 0 ? TowerType.VALUES[ordinalVal] : EMPTY;
 	}
 
 	/*********************************************************** Keys ********************************************************/
@@ -353,11 +489,11 @@ public enum TowerType implements StringRepresentable {
 
 	/*********************************************************** Extra ********************************************************/
 
-	public static AbstractDestructionEntity getDestructionEntity(TowerType towerType, Level level, BlockPos pos) {
+	public static AbstractDestructionEntity getDestructionEntity(TowerType towerType, Level level) {
 		return switch (towerType) {
-			case OCEAN -> new OceanDestructionEntity(level, pos);
-			case CORE -> new CoreDestructionEntity(level, pos);
-			default -> new LandDestructionEntity(level, pos);
+			case OCEAN -> new OceanDestructionEntity(level);
+			case CORE -> new CoreDestructionEntity(level);
+			default -> new LandDestructionEntity(level);
 		};
 	}
 
@@ -382,43 +518,6 @@ public enum TowerType implements StringRepresentable {
 			case NETHER -> EntityType.WITHER_SKELETON.create(serverLevel);
 			case END -> EntityType.ENDERMAN.create(serverLevel);
 			case SKY -> EntityType.SKELETON.create(serverLevel);
-			default -> null;
-		};
-	}
-
-	public static EntityType<?> getSpecialEnemyType(TowerType towerType) {
-		return switch (towerType) {
-			case LAND -> BTEntityType.BT_CULTIST.get();
-			case OCEAN -> EntityType.GUARDIAN;
-			case CORE -> EntityType.MAGMA_CUBE;
-			case NETHER -> EntityType.WITHER_SKELETON;
-			case END -> EntityType.ENDERMAN;
-			case SKY -> EntityType.SKELETON;
-			default -> null;
-		};
-	}
-
-	public static Class<? extends Entity> getSpecialEnemyClass(TowerType towerType) {
-		return switch (towerType) {
-			case LAND -> BTCultist.class;
-			case OCEAN -> Guardian.class;
-			case CORE -> MagmaCube.class;
-			case NETHER -> WitherSkeleton.class;
-			case END -> EnderMan.class;
-			case SKY -> Skeleton.class;
-			default -> null;
-		};
-	}
-
-	public static Integer getNumForType(TowerType towerType) {
-		return switch (towerType) {
-			case LAND -> 0;
-			case OCEAN -> 1;
-			case CORE -> 2;
-			case NETHER -> 3;
-			case END -> 4;
-			case SKY -> 5;
-			case CITY -> 6;
 			default -> null;
 		};
 	}
@@ -448,17 +547,6 @@ public enum TowerType implements StringRepresentable {
 		};
 	}
 
-	public static double getDestructionPercent(TowerType towerType) {
-		return switch (towerType) {
-			case LAND -> landTowerCrumblePercent;
-			case OCEAN -> oceanTowerCrumblePercent;
-			case CORE -> coreTowerCrumblePercent;
-			case NETHER -> netherTowerCrumblePercent;
-			case END -> endTowerCrumblePercent;
-			case SKY -> skyTowerCrumblePercent;
-			default -> 0;
-		};
-	}
 
 	public static double getMaxHealthFor(TowerType towerType) {
 		return switch (towerType) {
@@ -487,4 +575,8 @@ public enum TowerType implements StringRepresentable {
     public String getColorCode() {
         return colorCode;
     }
+
+    public record TowerFloor(int spawnerAmt, int minSpawnDelay, int maxSpawnDelay, int spawnCount, int maxNearbyEntities, int requiredPlayerRange, int spawnRange) {
+
+	}
 }
