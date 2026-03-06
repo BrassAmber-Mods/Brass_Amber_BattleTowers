69a40d64d91a1f1de669cafc22462e338a20759a
JMilamber <socky79.turbulance@gmail.com>
2026-03-05 23:53:57 -0500

    fix land spawner name issues?

--- src/main/java/com/brass_amber/ba_bt/init/BTBlockEntityType.java
+++ src/main/java/com/brass_amber/ba_bt/init/BTBlockEntityType.java
@@ -53,12 +53,12 @@ public class BTBlockEntityType {
 			"sky_golem_chest", () -> BlockEntityType.Builder.of(BTChestBlockEntity::new, BTBlocks.SKY_GOLEM_CHEST.get()).build(null)
 	);
 
-	public static final RegistryObject<BlockEntityType<BTLandSpawnerEntity>> LAND_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("l_spawner", () -> BlockEntityType.Builder.of(BTLandSpawnerEntity::new, BTBlocks.LAND_SPAWNER.get()).build(null));
-	public static final RegistryObject<BlockEntityType<BTOceanSpawnerEntity>> OCEAN_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("o_spawner", () -> BlockEntityType.Builder.of(BTOceanSpawnerEntity::new, BTBlocks.OCEAN_SPAWNER.get()).build(null));
-	public static final RegistryObject<BlockEntityType<BTCoreSpawnerEntity>> CORE_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("c_spawner", () -> BlockEntityType.Builder.of(BTCoreSpawnerEntity::new, BTBlocks.CORE_SPAWNER.get()).build(null));
-	public static final RegistryObject<BlockEntityType<BTNetherSpawnerEntity>> NETHER_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("n_spawner", () -> BlockEntityType.Builder.of(BTNetherSpawnerEntity::new, BTBlocks.NETHER_SPAWNER.get()).build(null));
-	public static final RegistryObject<BlockEntityType<BTEndSpawnerEntity>> END_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("e_spawner", () -> BlockEntityType.Builder.of(BTEndSpawnerEntity::new, BTBlocks.END_SPAWNER.get()).build(null));
-	public static final RegistryObject<BlockEntityType<BTSkySpawnerEntity>> SKY_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("s_spawner", () -> BlockEntityType.Builder.of(BTSkySpawnerEntity::new, BTBlocks.SKY_SPAWNER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<BTLandSpawnerEntity>> LAND_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("land_spawner", () -> BlockEntityType.Builder.of(BTLandSpawnerEntity::new, BTBlocks.LAND_SPAWNER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<BTOceanSpawnerEntity>> OCEAN_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("ocean_spawner", () -> BlockEntityType.Builder.of(BTOceanSpawnerEntity::new, BTBlocks.OCEAN_SPAWNER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<BTCoreSpawnerEntity>> CORE_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("core_spawner", () -> BlockEntityType.Builder.of(BTCoreSpawnerEntity::new, BTBlocks.CORE_SPAWNER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<BTNetherSpawnerEntity>> NETHER_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("nether_spawner", () -> BlockEntityType.Builder.of(BTNetherSpawnerEntity::new, BTBlocks.NETHER_SPAWNER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<BTEndSpawnerEntity>> END_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("end_spawner", () -> BlockEntityType.Builder.of(BTEndSpawnerEntity::new, BTBlocks.END_SPAWNER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<BTSkySpawnerEntity>> SKY_MOB_SPAWNER = BLOCK_ENTITY_TYPES.register("sky_spawner", () -> BlockEntityType.Builder.of(BTSkySpawnerEntity::new, BTBlocks.SKY_SPAWNER.get()).build(null));
 
 	public static final RegistryObject<BlockEntityType<DataMarkerBlockEntity>> DATA_MARKER = BLOCK_ENTITY_TYPES.register("data_marker", () -> BlockEntityType.Builder.of(DataMarkerBlockEntity::new, BTBlocks.DATA_MARKER.get()).build(null));
 
