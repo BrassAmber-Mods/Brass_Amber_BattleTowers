89ac8b12e3029117ba091cf1577149d9eb2eb4a2
JMilamber <socky79.turbulance@gmail.com>
2026-03-06 21:03:46 -0500

    add correye textures and model

---
+++ src/main/resources/assets/ba_bt/textures/entity/correye.java
@@ -0,0 +1,61 @@
+// Made with Blockbench 5.0.7
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+public class correye<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "correye"), "main");
+	private final ModelPart correye;
+	private final ModelPart body;
+	private final ModelPart tendril_right;
+	private final ModelPart tendril_left;
+	private final ModelPart tendril_middle;
+
+	public correye(ModelPart root) {
+		this.correye = root.getChild("correye");
+		this.body = this.correye.getChild("body");
+		this.tendril_right = this.body.getChild("tendril_right");
+		this.tendril_left = this.body.getChild("tendril_left");
+		this.tendril_middle = this.body.getChild("tendril_middle");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition correye = partdefinition.addOrReplaceChild("correye", CubeListBuilder.create(), PartPose.offset(0.0F, -2.0F, 0.0F));
+
+		PartDefinition body = correye.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -35.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 26.0F, 0.0F));
+
+		PartDefinition tendril_right = body.addOrReplaceChild("tendril_right", CubeListBuilder.create(), PartPose.offsetAndRotation(-6.0F, -27.0F, 7.0F, 0.7418F, 0.0F, 0.829F));
+
+		PartDefinition tendril_top_r1 = tendril_right.addOrReplaceChild("tendril_top_r1", CubeListBuilder.create().texOffs(30, 16).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.0F, 7.0F, 10.0F, -0.1309F, 0.0F, 0.0F));
+
+		PartDefinition tendril_bottom_r1 = tendril_right.addOrReplaceChild("tendril_bottom_r1", CubeListBuilder.create().texOffs(28, 16).addBox(-3.0F, -4.0F, -1.0F, 4.0F, 4.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.0F, 2.0F, 0.0F, -0.5236F, 0.0F, 0.0F));
+
+		PartDefinition tendril_left = body.addOrReplaceChild("tendril_left", CubeListBuilder.create(), PartPose.offsetAndRotation(6.0F, -27.0F, 7.0F, 0.0F, 0.0F, -0.9599F));
+
+		PartDefinition tendril_top_r2 = tendril_left.addOrReplaceChild("tendril_top_r2", CubeListBuilder.create().texOffs(30, 16).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.0F, 5.0F, 10.0F, -0.9599F, 0.0F, 0.0F));
+
+		PartDefinition tendril_bottom_r2 = tendril_left.addOrReplaceChild("tendril_bottom_r2", CubeListBuilder.create().texOffs(28, 16).addBox(-3.0F, -4.0F, -1.0F, 4.0F, 4.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.5236F, 0.0F, 0.0F));
+
+		PartDefinition tendril_middle = body.addOrReplaceChild("tendril_middle", CubeListBuilder.create(), PartPose.offset(1.0F, -26.0F, 7.0F));
+
+		PartDefinition tendril_top_r3 = tendril_middle.addOrReplaceChild("tendril_top_r3", CubeListBuilder.create().texOffs(30, 16).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 2.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.0F, 5.0F, 10.0F, -0.9599F, 0.0F, 0.0F));
+
+		PartDefinition tendril_bottom_r3 = tendril_middle.addOrReplaceChild("tendril_bottom_r3", CubeListBuilder.create().texOffs(28, 16).addBox(-3.0F, -4.0F, -1.0F, 4.0F, 4.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.5236F, 0.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+	@Override
+	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		correye.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
